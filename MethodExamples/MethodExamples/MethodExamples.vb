564b804df201e3aeeb22c8c03cc1991a85f0386f
rosstimo <rosstimo@isu.edu>
2022-01-26 08:54:27 -0700

    add Method Examples

---
+++ MethodExamples/MethodExamples/MethodExamples.vb
@@ -0,0 +1,37 @@
+﻿Option Explicit On
+
+Module MethodExamples
+
+    Sub Main()
+        Dim result As Integer
+        'Call Sub
+        AddTwoNumbers()
+        Console.WriteLine("In main")
+        Console.WriteLine(result)
+
+        SumOf(4, 6)
+
+
+        'SayAMessage()
+
+        Console.Read()
+    End Sub
+
+    Sub AddTwoNumbers()
+        Dim result As Integer
+        result = 2 + 2
+        Console.WriteLine("In Sub")
+        Console.WriteLine(result)
+    End Sub
+
+    Sub SumOf(firstNumber As Integer, secondNumber As Integer)
+
+    End Sub
+
+    Sub SayAMessage()
+        Console.WriteLine("Hello....")
+    End Sub
+
+
+
+End Module
