738bb102e14170043a817519370e6c428d67e21a
rosstimo <rosstimo@isu.edu>
2022-01-27 08:40:56 -0700

    add console menu examples

--- ConsoleExamples/ConsoleExamples/ConsoleExamples.vb
+++ ConsoleExamples/ConsoleExamples/ConsoleExamples.vb
@@ -7,13 +7,39 @@ Module ConsoleExamples
         Dim firstNumber As Integer = 4
         Dim secondNumber As Integer
         Dim result As Integer
+        Dim menu As String
 
         'Console.WriteLine("Please type something and press enter...")
         'userResponse = Console.ReadLine()
         'Console.WriteLine("You entered: " & userResponse)
 
-        Console.WriteLine("Please enter a number")
+        Console.WriteLine("Please enter a number or Type " & Chr(34) & "Q" & Chr(34) & " to quit")
+        Console.WriteLine("Please enter a number or Type ""Q"" to quit")
+
+        menu = "1. Add" & vbNewLine _
+             & "2. Subtract" & vbNewLine _
+             & "3. Multiply"
+
+        'menu = $"1. Add{vbNewLine}2. Subtract{vbNewLine}3. Multiply"
+        '        menu = $"
+        '1. Add
+        '2. Subtract
+        '3. Multiply"
+        Console.WriteLine(menu)
+
         userResponse = Console.ReadLine()
+
+        Select Case userResponse
+            Case "1"
+                Console.WriteLine("2 + 2 = 4")
+            Case "2"
+                Console.WriteLine("5 - 3 = 2")
+            Case "3"
+                Console.WriteLine("3 x 5 = 15")
+            Case Else
+                Console.WriteLine("oops, invalid option")
+        End Select
+
 
         If userResponse = "3" Then
             Console.WriteLine("Three is my favorite number! yippee!!!!")
