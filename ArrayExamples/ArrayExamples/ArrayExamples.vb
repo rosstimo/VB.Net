2c5f4f35b244db4d697d09a69387c14d8fa3d32e
rosstimo <rosstimo@isu.edu>
2022-02-10 08:36:25 -0700

    add simple array examples

---
+++ ArrayExamples/ArrayExamples/ArrayExamples.vb
@@ -0,0 +1,48 @@
+﻿Module ArrayExamples
+
+    Sub Main()
+
+
+
+        Console.ReadLine()
+
+    End Sub
+
+    Sub SimpleArray()
+        'Declare an array of strings with 0 to 5 elements
+        Dim names(5) As String
+
+        'add data to the array
+        names(0) = "Jim"
+        names(1) = "Pual"
+        names(2) = "Mary"
+        names(4) = "Sally"
+
+        'exception
+        'names(6) = "xxx"
+
+        'access an element
+        Console.WriteLine(names(2))
+
+    End Sub
+
+    Sub TwoDimensionArrayLoop()
+        Dim numbers(9, 9) As Integer
+        Dim temp As String
+
+        For row = 0 To 9
+            For column = 0 To 9
+                numbers(row, column) = row + column
+            Next
+        Next
+
+        For row = 0 To 9
+            For column = 0 To 9
+                temp = CStr(numbers(row, column)) & " |"
+                Console.Write(temp.PadLeft(6))
+            Next
+            Console.WriteLine()
+        Next
+    End Sub
+
+End Module
---
+++ ArrayExamples/ArrayExamples/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("ArrayExamples")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("ArrayExamples")>
+<Assembly: AssemblyCopyright("Copyright ©  2022")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("749b846f-3406-420d-9bf9-48ba2bd32289")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
