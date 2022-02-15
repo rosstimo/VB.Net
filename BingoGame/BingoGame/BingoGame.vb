3dbbc17b079abe23aca0f31603884f355668ffc3
rosstimo <rosstimo@isu.edu>
2022-02-15 08:53:53 -0700

    init

---
+++ BingoGame/BingoGame/BingoGame.vb
@@ -0,0 +1,11 @@
+﻿Module BingoGame
+    'TODO how many balls for each letter
+    'TODO draw a random ball Letter - B - I - N - G - O - with number
+    'TODO track what balls have come up in an array
+    'TODO don't draw duplicates
+    'TODO clear tracking history on new game
+    Sub Main()
+
+    End Sub
+
+End Module
