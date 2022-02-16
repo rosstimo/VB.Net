f81597eff9bb8e8fb72f58a93fd2f78cf1da14a3
rosstimo <rosstimo@isu.edu>
2022-02-16 15:28:14 -0700

    working display bingo cage

--- BingoGame/BingoGame/BingoGame.vb
+++ BingoGame/BingoGame/BingoGame.vb
@@ -12,6 +12,9 @@
 
     Sub Main()
         Dim bingoCage(4, 14) As Boolean
+        bingoCage(0, 0) = True
+        bingoCage(4, 4) = True
+        bingoCage(4, 14) = True
         DisplayBingoCage(bingoCage)
         Console.Read()
     End Sub
@@ -21,17 +24,16 @@
         Dim columnWidth As Integer = 3
         Dim columnData As String
 
-        For row = bingoCage.GetLength(1) To 0 Step -1
-
-            For column = 1 To bingoCage.GetLength(0)
+        For row = 0 To bingoCage.GetLength(1)
+            For column = 0 To bingoCage.GetLength(0) - 1
                 Select Case row
                     Case 0
-                        columnData = header(column - 1).PadLeft(columnWidth)
+                        columnData = header(column).PadLeft(columnWidth)
                     Case Else
-                        If bingoCage(column - 1, row - 1) Then
+                        If bingoCage(column, row - 1) Then
                             columnData = "X"
                         Else
-                            columnData = CStr((column * bingoCage.GetLength(1)) - row + 1)
+                            columnData = CStr(((column) * bingoCage.GetLength(1)) + row)
                         End If
                 End Select
                 Console.Write(columnData.PadLeft(columnWidth))
@@ -41,5 +43,21 @@
 
     End Sub
 
-
+    ''' <summary>
+    ''' The default range is 0 - 10.
+    ''' The maximum number must be greater than minimum number.
+    ''' </summary>
+    ''' <param name="max%"></param>
+    ''' <param name="min%"></param>
+    ''' <returns>Returns a random integer within a range defined by the max and min arguments.</returns>
+    ''' <exception cref="System.ArgumentException">Thrown when <c>max > min</c></exception>
+    Function RandomNumberInRange(Optional max% = 10%, Optional min% = 0%) As Integer
+        Dim _max% = max - min
+        If _max < 0 Then
+            Throw New System.ArgumentException("Maximum number must be greater than minimum number")
+        End If
+        Randomize(DateTime.Now.Millisecond)
+        Return CInt(System.Math.Floor(Rnd() * (_max + 1))) + min
+    End Function
 End Module
+
