d19e97b1c830e73c711265ad9225e3d5f3285cc5
rosstimo <rosstimo@isu.edu>
2022-02-24 08:14:21 -0700

    wow a lot

---
+++ PlayinWithStrings/PlayinWithStrings/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("PlayinWithStrings")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("PlayinWithStrings")>
+<Assembly: AssemblyCopyright("Copyright ©  2022")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("68ca2164-ac10-4e05-8369-9c4206b3ba5f")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
---
+++ PlayinWithStrings/PlayinWithStrings/PlayinWithStrings.vb
@@ -0,0 +1,47 @@
+﻿'see:
+'https://docs.microsoft.com/en-us/dotnet/api/microsoft.visualbasic.strings?view=netframework-4.8#methods
+
+Option Strict On
+Option Explicit On
+Option Compare Binary
+
+
+Module PlayinWithStrings
+
+    Sub Main()
+        Dim testString As String = "Hello, World!!"
+        Dim money As Decimal = 1426.876273897289@
+        Dim length As Integer
+        Dim temp() As String
+
+
+        temp = Split(testString, ", ")
+        Console.WriteLine(temp(1))
+
+        'testString = Left(testString, 7)
+        'testString = Right(testString, 2)
+        'testString = Mid(testString, 5)
+        'testString = Left(testString, 4)
+
+
+        'For i = Len(testString) - 1 To 0 Step -1
+        '    Console.Write(testString(i))
+        'Next
+        'Console.WriteLine()
+
+        'Console.WriteLine(StrReverse(testString))
+        'Console.WriteLine(UCase(testString))
+        'Console.WriteLine(LCase(testString))
+        'testString = Replace(testString, "W", "X")
+
+
+        'Console.WriteLine(testString)
+        'Console.WriteLine(money.ToString("C"))
+
+        Console.Read()
+    End Sub
+
+
+
+
+End Module
