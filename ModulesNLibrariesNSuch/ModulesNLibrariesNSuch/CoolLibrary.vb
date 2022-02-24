3b23b626a1ec2da9e94326c2fad4f898debd37a1
rosstimo <rosstimo@isu.edu>
2022-02-24 08:54:50 -0700

    add some cool functions to the CoolLibrary

---
+++ ModulesNLibrariesNSuch/ModulesNLibrariesNSuch/CoolLibrary.vb
@@ -0,0 +1,27 @@
+﻿Module CoolLibrary
+
+    ''' <summary>
+    ''' A full featured function to say hello...
+    ''' </summary>
+    ''' <returns>A string "Hello, World!"</returns>
+    Public Function SayHello() As String
+        Return "Hello, World!"
+    End Function
+
+    ''' <summary>
+    ''' Displays the contents of argument displayMessage then returns the user's response. 
+    ''' </summary>
+    ''' <param name="displayMessage"></param>
+    ''' <returns>String: user response</returns>
+    Public Function Prompt(ByVal Optional displayMessage As String = "") As String
+        Console.WriteLine(displayMessage)
+        Return Console.ReadLine
+    End Function
+
+    Public Function RandomWithInRange(max As Integer, min As Integer) As Integer
+        max = max - min
+        Randomize(DateTime.Now.Millisecond)
+        Return CInt(Rnd() * max) + min
+    End Function
+
+End Module
--- ModulesNLibrariesNSuch/ModulesNLibrariesNSuch/MyAwesomeApp.vb
+++ ModulesNLibrariesNSuch/ModulesNLibrariesNSuch/MyAwesomeApp.vb
@@ -1,7 +1,22 @@
-﻿Module MyAwesomeApp
+﻿
+Imports CoolLibrary
+
+Module MyAwesomeApp
 
     Sub Main()
+        Dim temp As String
+        Dim menu As String
 
+        'menu = "1. Delete" & vbNewLine _
+        '     & "2. Save" & vbNewLine _
+        '     & "3. Rename" & vbNewLine
+
+        'temp = Prompt(menu)
+
+        Prompt()
+
+        'Console.WriteLine(temp)
+        Console.Read()
     End Sub
 
 End Module
