653e6190e59c6186c4bdecba2fc848437ff679ba
rosstimo <rosstimo@isu.edu>
2022-03-02 08:42:44 -0700

    add control examples

--- WinFormExample/WinFormExample/WinFormExampleForm.Designer.vb
+++ WinFormExample/WinFormExample/WinFormExampleForm.Designer.vb
@@ -22,10 +22,65 @@ Partial Class WinFormExampleForm
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.ExitButton = New System.Windows.Forms.Button()
+        Me.GoButton = New System.Windows.Forms.Button()
+        Me.NameLabel = New System.Windows.Forms.Label()
+        Me.NameTextBox = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'ExitButton
+        '
+        Me.ExitButton.Location = New System.Drawing.Point(284, 259)
+        Me.ExitButton.Name = "ExitButton"
+        Me.ExitButton.Size = New System.Drawing.Size(166, 83)
+        Me.ExitButton.TabIndex = 0
+        Me.ExitButton.Text = "E&xit"
+        Me.ExitButton.UseVisualStyleBackColor = True
+        '
+        'GoButton
+        '
+        Me.GoButton.Location = New System.Drawing.Point(112, 259)
+        Me.GoButton.Name = "GoButton"
+        Me.GoButton.Size = New System.Drawing.Size(166, 83)
+        Me.GoButton.TabIndex = 1
+        Me.GoButton.Text = "&Go"
+        Me.GoButton.UseVisualStyleBackColor = True
+        '
+        'NameLabel
+        '
+        Me.NameLabel.AutoSize = True
+        Me.NameLabel.Location = New System.Drawing.Point(17, 35)
+        Me.NameLabel.Name = "NameLabel"
+        Me.NameLabel.Size = New System.Drawing.Size(35, 13)
+        Me.NameLabel.TabIndex = 2
+        Me.NameLabel.Text = "Name"
+        '
+        'NameTextBox
+        '
+        Me.NameTextBox.Location = New System.Drawing.Point(58, 32)
+        Me.NameTextBox.Name = "NameTextBox"
+        Me.NameTextBox.Size = New System.Drawing.Size(237, 20)
+        Me.NameTextBox.TabIndex = 3
+        '
+        'WinFormExampleForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form1"
+        Me.ClientSize = New System.Drawing.Size(462, 354)
+        Me.Controls.Add(Me.NameTextBox)
+        Me.Controls.Add(Me.NameLabel)
+        Me.Controls.Add(Me.GoButton)
+        Me.Controls.Add(Me.ExitButton)
+        Me.Name = "WinFormExampleForm"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Hello"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents ExitButton As Button
+    Friend WithEvents GoButton As Button
+    Friend WithEvents NameLabel As Label
+    Friend WithEvents NameTextBox As TextBox
 End Class
--- WinFormExample/WinFormExample/WinFormExampleForm.vb
+++ WinFormExample/WinFormExample/WinFormExampleForm.vb
@@ -1,3 +1,11 @@
 ﻿Public Class WinFormExampleForm
+    Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
+        Me.Close()
+    End Sub
 
+    Private Sub GoButton_Click(sender As Object, e As EventArgs) Handles GoButton.Click
+        'Me.Text = "You have clicked the button"
+        Me.Text = NameTextBox.Text
+
+    End Sub
 End Class
