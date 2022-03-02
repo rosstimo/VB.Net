1f32b512f36e1e635915ef6eb1a0f73b444e3ebb
rosstimo <rosstimo@isu.edu>
2022-03-02 08:14:14 -0700

    rename form

--- WinFormExample/WinFormExample/My Project/Application.Designer.vb
+++ WinFormExample/WinFormExample/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
 
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()> _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.WinFormExample.Form1
+            Me.MainForm = Global.WinFormExample.WinFormExampleForm
         End Sub
     End Class
 End Namespace
