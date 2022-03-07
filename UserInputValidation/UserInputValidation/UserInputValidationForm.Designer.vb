8c2122237b88d9bd6b3172e28b2132b2487081ab
rosstimo <rosstimo@isu.edu>
2022-03-07 08:54:58 -0700

    add user validation form example

---
+++ UserInputValidation/UserInputValidation/UserInputValidationForm.Designer.vb
@@ -0,0 +1,190 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class UserInputValidationForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.FirstNameLabel = New System.Windows.Forms.Label()
+        Me.QuitButton = New System.Windows.Forms.Button()
+        Me.FirstNameTextBox = New System.Windows.Forms.TextBox()
+        Me.InputGroupBox = New System.Windows.Forms.GroupBox()
+        Me.OutputGroupBox = New System.Windows.Forms.GroupBox()
+        Me.ButtonGroupBox = New System.Windows.Forms.GroupBox()
+        Me.ClearButton = New System.Windows.Forms.Button()
+        Me.SubmitButton = New System.Windows.Forms.Button()
+        Me.LastNameLabel = New System.Windows.Forms.Label()
+        Me.LastNameTextBox = New System.Windows.Forms.TextBox()
+        Me.AgeLabel = New System.Windows.Forms.Label()
+        Me.AgeTextBox = New System.Windows.Forms.TextBox()
+        Me.ResultLabel = New System.Windows.Forms.Label()
+        Me.InputGroupBox.SuspendLayout()
+        Me.OutputGroupBox.SuspendLayout()
+        Me.ButtonGroupBox.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'FirstNameLabel
+        '
+        Me.FirstNameLabel.Location = New System.Drawing.Point(16, 20)
+        Me.FirstNameLabel.Name = "FirstNameLabel"
+        Me.FirstNameLabel.Size = New System.Drawing.Size(35, 14)
+        Me.FirstNameLabel.TabIndex = 0
+        Me.FirstNameLabel.Text = "First"
+        '
+        'QuitButton
+        '
+        Me.QuitButton.Location = New System.Drawing.Point(445, 47)
+        Me.QuitButton.Name = "QuitButton"
+        Me.QuitButton.Size = New System.Drawing.Size(120, 74)
+        Me.QuitButton.TabIndex = 1
+        Me.QuitButton.Text = "E&xit"
+        Me.QuitButton.UseVisualStyleBackColor = True
+        '
+        'FirstNameTextBox
+        '
+        Me.FirstNameTextBox.Location = New System.Drawing.Point(57, 20)
+        Me.FirstNameTextBox.Name = "FirstNameTextBox"
+        Me.FirstNameTextBox.Size = New System.Drawing.Size(169, 20)
+        Me.FirstNameTextBox.TabIndex = 2
+        '
+        'InputGroupBox
+        '
+        Me.InputGroupBox.Controls.Add(Me.AgeLabel)
+        Me.InputGroupBox.Controls.Add(Me.AgeTextBox)
+        Me.InputGroupBox.Controls.Add(Me.LastNameLabel)
+        Me.InputGroupBox.Controls.Add(Me.LastNameTextBox)
+        Me.InputGroupBox.Controls.Add(Me.FirstNameLabel)
+        Me.InputGroupBox.Controls.Add(Me.FirstNameTextBox)
+        Me.InputGroupBox.Location = New System.Drawing.Point(13, 35)
+        Me.InputGroupBox.Name = "InputGroupBox"
+        Me.InputGroupBox.Size = New System.Drawing.Size(282, 227)
+        Me.InputGroupBox.TabIndex = 0
+        Me.InputGroupBox.TabStop = False
+        '
+        'OutputGroupBox
+        '
+        Me.OutputGroupBox.Controls.Add(Me.ResultLabel)
+        Me.OutputGroupBox.Location = New System.Drawing.Point(301, 35)
+        Me.OutputGroupBox.Name = "OutputGroupBox"
+        Me.OutputGroupBox.Size = New System.Drawing.Size(282, 227)
+        Me.OutputGroupBox.TabIndex = 4
+        Me.OutputGroupBox.TabStop = False
+        '
+        'ButtonGroupBox
+        '
+        Me.ButtonGroupBox.Controls.Add(Me.SubmitButton)
+        Me.ButtonGroupBox.Controls.Add(Me.ClearButton)
+        Me.ButtonGroupBox.Controls.Add(Me.QuitButton)
+        Me.ButtonGroupBox.Location = New System.Drawing.Point(12, 268)
+        Me.ButtonGroupBox.Name = "ButtonGroupBox"
+        Me.ButtonGroupBox.Size = New System.Drawing.Size(571, 127)
+        Me.ButtonGroupBox.TabIndex = 0
+        Me.ButtonGroupBox.TabStop = False
+        '
+        'ClearButton
+        '
+        Me.ClearButton.Location = New System.Drawing.Point(319, 47)
+        Me.ClearButton.Name = "ClearButton"
+        Me.ClearButton.Size = New System.Drawing.Size(120, 74)
+        Me.ClearButton.TabIndex = 2
+        Me.ClearButton.Text = "&Clear"
+        Me.ClearButton.UseVisualStyleBackColor = True
+        '
+        'SubmitButton
+        '
+        Me.SubmitButton.Location = New System.Drawing.Point(193, 47)
+        Me.SubmitButton.Name = "SubmitButton"
+        Me.SubmitButton.Size = New System.Drawing.Size(120, 74)
+        Me.SubmitButton.TabIndex = 3
+        Me.SubmitButton.Text = "&Submit"
+        Me.SubmitButton.UseVisualStyleBackColor = True
+        '
+        'LastNameLabel
+        '
+        Me.LastNameLabel.Location = New System.Drawing.Point(16, 46)
+        Me.LastNameLabel.Name = "LastNameLabel"
+        Me.LastNameLabel.Size = New System.Drawing.Size(35, 17)
+        Me.LastNameLabel.TabIndex = 3
+        Me.LastNameLabel.Text = "Last"
+        '
+        'LastNameTextBox
+        '
+        Me.LastNameTextBox.Location = New System.Drawing.Point(57, 46)
+        Me.LastNameTextBox.Name = "LastNameTextBox"
+        Me.LastNameTextBox.Size = New System.Drawing.Size(169, 20)
+        Me.LastNameTextBox.TabIndex = 4
+        '
+        'AgeLabel
+        '
+        Me.AgeLabel.AutoSize = True
+        Me.AgeLabel.Location = New System.Drawing.Point(16, 72)
+        Me.AgeLabel.Name = "AgeLabel"
+        Me.AgeLabel.Size = New System.Drawing.Size(26, 13)
+        Me.AgeLabel.TabIndex = 5
+        Me.AgeLabel.Text = "Age"
+        '
+        'AgeTextBox
+        '
+        Me.AgeTextBox.Location = New System.Drawing.Point(57, 72)
+        Me.AgeTextBox.Name = "AgeTextBox"
+        Me.AgeTextBox.Size = New System.Drawing.Size(169, 20)
+        Me.AgeTextBox.TabIndex = 6
+        '
+        'ResultLabel
+        '
+        Me.ResultLabel.Location = New System.Drawing.Point(27, 27)
+        Me.ResultLabel.Name = "ResultLabel"
+        Me.ResultLabel.Size = New System.Drawing.Size(227, 65)
+        Me.ResultLabel.TabIndex = 6
+        Me.ResultLabel.Text = "Result"
+        '
+        'UserInputValidationForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(595, 408)
+        Me.Controls.Add(Me.ButtonGroupBox)
+        Me.Controls.Add(Me.OutputGroupBox)
+        Me.Controls.Add(Me.InputGroupBox)
+        Me.Name = "UserInputValidationForm"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Input Validation"
+        Me.InputGroupBox.ResumeLayout(False)
+        Me.InputGroupBox.PerformLayout()
+        Me.OutputGroupBox.ResumeLayout(False)
+        Me.ButtonGroupBox.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents FirstNameLabel As Label
+    Friend WithEvents QuitButton As Button
+    Friend WithEvents FirstNameTextBox As TextBox
+    Friend WithEvents InputGroupBox As GroupBox
+    Friend WithEvents AgeLabel As Label
+    Friend WithEvents AgeTextBox As TextBox
+    Friend WithEvents LastNameLabel As Label
+    Friend WithEvents LastNameTextBox As TextBox
+    Friend WithEvents OutputGroupBox As GroupBox
+    Friend WithEvents ResultLabel As Label
+    Friend WithEvents ButtonGroupBox As GroupBox
+    Friend WithEvents SubmitButton As Button
+    Friend WithEvents ClearButton As Button
+End Class
---
+++ UserInputValidation/UserInputValidation/UserInputValidationForm.vb
@@ -0,0 +1,72 @@
+﻿Public Class UserInputValidationForm
+
+    Sub ValidateInput()
+        Dim userAge As Integer
+
+        'If AgeTextBox.Text = "" Then
+        '    AccumulateMessages("Age is required")
+        '    AgeTextBox.Focus()
+        'End If
+
+        Try
+            userAge = CInt(AgeTextBox.Text)
+        Catch ex As Exception
+            AccumulateMessages("Age must be a number")
+            AgeTextBox.Focus()
+        End Try
+
+        If LastNameTextBox.Text = "" Then
+            AccumulateMessages("Last name is required")
+            LastNameTextBox.Focus()
+        End If
+
+        If FirstNameTextBox.Text = "" Then
+            AccumulateMessages("First name is required")
+            FirstNameTextBox.Focus()
+        End If
+
+        If AccumulateMessages() <> "" Then
+            MsgBox(AccumulateMessages())
+            AccumulateMessages(, True)
+        End If
+    End Sub
+
+    Private Function AccumulateMessages(Optional newMessage As String = "", Optional clear As Boolean = False) As String
+        Static _message As String
+
+        Select Case clear
+            Case False
+                If newMessage <> "" Then
+                    _message &= newMessage & vbCrLf
+                End If
+            Case Else
+                _message = ""
+        End Select
+
+        Return _message
+    End Function
+
+    Private Sub Reset()
+
+        'reset all form controls to default
+        FirstNameTextBox.Text = ""
+        LastNameTextBox.Text = ""
+        AgeTextBox.Text = ""
+        ResultLabel.Text = ""
+        'Clear any stored messages
+        AccumulateMessages(, True)
+
+    End Sub
+
+    Private Sub SubmitButton_Click(sender As Object, e As EventArgs) Handles SubmitButton.Click
+        ValidateInput()
+    End Sub
+
+    Private Sub QuitButton_Click(sender As Object, e As EventArgs) Handles QuitButton.Click
+        Me.Close()
+    End Sub
+
+    Private Sub ClearButton_Click(sender As Object, e As EventArgs) Handles ClearButton.Click
+        Reset()
+    End Sub
+End Class
