6900648b1f0dbcec189dda963ef9d0f6e1f9c6f7
rosstimo <rosstimo@isu.edu>
2022-03-30 08:56:18 -0600

    add old file IO example

---
+++ FileIOExample/FileIOExample/FileIOForm.designer.vb
@@ -0,0 +1,209 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FileIOForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.ReadFileButton = New System.Windows.Forms.Button()
+        Me.AppendFileButton = New System.Windows.Forms.Button()
+        Me.OverWriteButton = New System.Windows.Forms.Button()
+        Me.ExitButton = New System.Windows.Forms.Button()
+        Me.FirstNameTextBox = New System.Windows.Forms.TextBox()
+        Me.LastNameTextBox = New System.Windows.Forms.TextBox()
+        Me.StateTextBox = New System.Windows.Forms.TextBox()
+        Me.ZipTextBox = New System.Windows.Forms.TextBox()
+        Me.SaveFileDialog = New System.Windows.Forms.SaveFileDialog()
+        Me.OpenFileDialog = New System.Windows.Forms.OpenFileDialog()
+        Me.SaveFileButton = New System.Windows.Forms.Button()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.emailTextBox = New System.Windows.Forms.TextBox()
+        Me.CityTextBox = New System.Windows.Forms.TextBox()
+        Me.UpdateButton = New System.Windows.Forms.Button()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(218, 12)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(378, 368)
+        Me.ListBox1.TabIndex = 0
+        '
+        'ReadFileButton
+        '
+        Me.ReadFileButton.Location = New System.Drawing.Point(130, 383)
+        Me.ReadFileButton.Name = "ReadFileButton"
+        Me.ReadFileButton.Size = New System.Drawing.Size(112, 55)
+        Me.ReadFileButton.TabIndex = 1
+        Me.ReadFileButton.Text = "Read File"
+        Me.ReadFileButton.UseVisualStyleBackColor = True
+        '
+        'AppendFileButton
+        '
+        Me.AppendFileButton.Location = New System.Drawing.Point(248, 383)
+        Me.AppendFileButton.Name = "AppendFileButton"
+        Me.AppendFileButton.Size = New System.Drawing.Size(112, 55)
+        Me.AppendFileButton.TabIndex = 2
+        Me.AppendFileButton.Text = "Append File"
+        Me.AppendFileButton.UseVisualStyleBackColor = True
+        '
+        'OverWriteButton
+        '
+        Me.OverWriteButton.Location = New System.Drawing.Point(366, 383)
+        Me.OverWriteButton.Name = "OverWriteButton"
+        Me.OverWriteButton.Size = New System.Drawing.Size(112, 55)
+        Me.OverWriteButton.TabIndex = 3
+        Me.OverWriteButton.Text = "Write File"
+        Me.OverWriteButton.UseVisualStyleBackColor = True
+        '
+        'ExitButton
+        '
+        Me.ExitButton.Location = New System.Drawing.Point(484, 383)
+        Me.ExitButton.Name = "ExitButton"
+        Me.ExitButton.Size = New System.Drawing.Size(112, 55)
+        Me.ExitButton.TabIndex = 4
+        Me.ExitButton.Text = "Exit"
+        Me.ExitButton.UseVisualStyleBackColor = True
+        '
+        'FirstNameTextBox
+        '
+        Me.FirstNameTextBox.Location = New System.Drawing.Point(6, 29)
+        Me.FirstNameTextBox.Name = "FirstNameTextBox"
+        Me.FirstNameTextBox.Size = New System.Drawing.Size(170, 20)
+        Me.FirstNameTextBox.TabIndex = 5
+        Me.FirstNameTextBox.Text = "First"
+        '
+        'LastNameTextBox
+        '
+        Me.LastNameTextBox.Location = New System.Drawing.Point(6, 55)
+        Me.LastNameTextBox.Name = "LastNameTextBox"
+        Me.LastNameTextBox.Size = New System.Drawing.Size(170, 20)
+        Me.LastNameTextBox.TabIndex = 6
+        Me.LastNameTextBox.Text = "Last"
+        '
+        'StateTextBox
+        '
+        Me.StateTextBox.Location = New System.Drawing.Point(6, 107)
+        Me.StateTextBox.Name = "StateTextBox"
+        Me.StateTextBox.Size = New System.Drawing.Size(41, 20)
+        Me.StateTextBox.TabIndex = 8
+        Me.StateTextBox.Text = "State"
+        '
+        'ZipTextBox
+        '
+        Me.ZipTextBox.Location = New System.Drawing.Point(6, 133)
+        Me.ZipTextBox.Name = "ZipTextBox"
+        Me.ZipTextBox.Size = New System.Drawing.Size(72, 20)
+        Me.ZipTextBox.TabIndex = 9
+        Me.ZipTextBox.Text = "Zip"
+        '
+        'OpenFileDialog
+        '
+        Me.OpenFileDialog.FileName = "OpenFileDialog"
+        '
+        'SaveFileButton
+        '
+        Me.SaveFileButton.Location = New System.Drawing.Point(12, 383)
+        Me.SaveFileButton.Name = "SaveFileButton"
+        Me.SaveFileButton.Size = New System.Drawing.Size(112, 55)
+        Me.SaveFileButton.TabIndex = 10
+        Me.SaveFileButton.Text = "Save File"
+        Me.SaveFileButton.UseVisualStyleBackColor = True
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.emailTextBox)
+        Me.GroupBox1.Controls.Add(Me.ZipTextBox)
+        Me.GroupBox1.Controls.Add(Me.StateTextBox)
+        Me.GroupBox1.Controls.Add(Me.FirstNameTextBox)
+        Me.GroupBox1.Controls.Add(Me.LastNameTextBox)
+        Me.GroupBox1.Controls.Add(Me.CityTextBox)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(200, 208)
+        Me.GroupBox1.TabIndex = 11
+        Me.GroupBox1.TabStop = False
+        '
+        'emailTextBox
+        '
+        Me.emailTextBox.Location = New System.Drawing.Point(6, 159)
+        Me.emailTextBox.Name = "emailTextBox"
+        Me.emailTextBox.Size = New System.Drawing.Size(72, 20)
+        Me.emailTextBox.TabIndex = 10
+        Me.emailTextBox.Text = "email"
+        '
+        'CityTextBox
+        '
+        Me.CityTextBox.Location = New System.Drawing.Point(6, 81)
+        Me.CityTextBox.Name = "CityTextBox"
+        Me.CityTextBox.Size = New System.Drawing.Size(170, 20)
+        Me.CityTextBox.TabIndex = 7
+        Me.CityTextBox.Text = "City"
+        '
+        'UpdateButton
+        '
+        Me.UpdateButton.Location = New System.Drawing.Point(12, 226)
+        Me.UpdateButton.Name = "UpdateButton"
+        Me.UpdateButton.Size = New System.Drawing.Size(112, 55)
+        Me.UpdateButton.TabIndex = 12
+        Me.UpdateButton.Text = "Update"
+        Me.UpdateButton.UseVisualStyleBackColor = True
+        '
+        'FileIOForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(616, 450)
+        Me.Controls.Add(Me.UpdateButton)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.SaveFileButton)
+        Me.Controls.Add(Me.ExitButton)
+        Me.Controls.Add(Me.OverWriteButton)
+        Me.Controls.Add(Me.AppendFileButton)
+        Me.Controls.Add(Me.ReadFileButton)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Name = "FileIOForm"
+        Me.Text = "`File Stuff"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents ReadFileButton As Button
+    Friend WithEvents AppendFileButton As Button
+    Friend WithEvents OverWriteButton As Button
+    Friend WithEvents ExitButton As Button
+    Friend WithEvents FirstNameTextBox As TextBox
+    Friend WithEvents LastNameTextBox As TextBox
+    Friend WithEvents StateTextBox As TextBox
+    Friend WithEvents ZipTextBox As TextBox
+    Friend WithEvents SaveFileDialog As SaveFileDialog
+    Friend WithEvents OpenFileDialog As OpenFileDialog
+    Friend WithEvents SaveFileButton As Button
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents CityTextBox As TextBox
+    Friend WithEvents emailTextBox As TextBox
+    Friend WithEvents UpdateButton As Button
+End Class
---
+++ FileIOExample/FileIOExample/FileIOForm.vb
@@ -0,0 +1,352 @@
+﻿'see lecture notes pdf: https://elearn.isu.edu/moodle/pluginfile.php/2000753/mod_resource/content/0/Section%2010%20Topics.pdf
+'see also https://docs.microsoft.com/en-us/dotnet/visual-basic/developing-apps/programming/drives-directories-files/basics-of-net-framework-file-io-and-the-file-system
+'https://docs.microsoft.com/en-us/dotnet/visual-basic/developing-apps/programming/drives-directories-files/file-access
+'FileOpen Depricated 
+'https://docs.microsoft.com/en-us/dotnet/api/microsoft.visualbasic.filesystem?view=netframework-4.8#methods
+'https://docs.microsoft.com/en-us/dotnet/api/microsoft.visualbasic.filesystem.fileopen?view=netframework-4.8
+'See current vb fileIO here:
+'https://docs.microsoft.com/en-us/dotnet/api/microsoft.visualbasic.fileio?view=netframework-4.8
+
+Option Explicit On
+Option Strict On
+Option Compare Binary
+
+
+'<Remarks>
+'The FileOpen() functions allows for various file read/write options.
+'FreeFile() returns the next available file number. It is used to refer to
+'the to the file by the FileOpen function. File IO can throw a variety of 
+'exceptions so Try/Catch Is used. Open file for input or read mode and 
+'assigned a file number. Loop through all the records/lines until end of
+'file or EOF().Input reads each record from File into currentRecord. The
+'currentRecord is then added to the list box. Always close the file to
+'avoid file In use IOException. Note: large files can be cumbersome when 
+'the whole thing Is read into RAM. Options? Check target file size 
+'attribute prior to read all and/or read one line at a time. Newer.NET
+'file IO classes offer more efficient and elegant solutions.
+'</Remarks>
+Public Class FileIOForm
+
+    Dim TempFileName As String = "TempFile.txt" '= My.Resources.TempFile_txt 'variable assignment not working at class level
+    Dim TempArray() As String
+
+
+    Private Sub DataCrossWalk()
+
+        Dim originalFileName As String = "C:\Users\rosstimo\Documents\GitHub\TJR-VS-F19\ClassRmExamples\FileIO_Examples\Resources\email.txt"
+        Dim newFileName As String = "C:\Users\rosstimo\Documents\GitHub\TJR-VS-F19\ClassRmExamples\FileIO_Examples\Resources\NEWemail.txt"
+        Dim crossWalkArray() As String
+        Dim recordData() As String
+        Dim goodDataString As String = ""
+
+
+        ReadFile(originalFileName, crossWalkArray)
+
+        For i = LBound(crossWalkArray) To UBound(crossWalkArray)
+            recordData = Split(crossWalkArray(i), ",")
+            Try
+                'goodDataString &= recordData(0) & "," & recordData(1) & "," & recordData(2) & "," & "ID" & "," & "" & "," & recordData(3) & "$$"
+                AppendFile(newFileName, "$$" & recordData(0) & "," & recordData(1) & "," & recordData(2) & "," & "ID" & "," & "" & "," & recordData(3))
+                Console.WriteLine(crossWalkArray(i))
+            Catch ex As Exception
+                Console.WriteLine(ex.Message & " *********** Hey You! ***************   Index #: " & Str(i))
+            End Try
+        Next
+
+        crossWalkArray = Split(goodDataString, "$$")
+
+        'If crossWalkArray(0) = "" Then
+        '    Console.WriteLine(crossWalkArray(0))
+        'Else
+        '    Console.WriteLine("Nope")
+        'End If
+
+        'Console.WriteLine(Len(crossWalkArray(0)))
+        'Console.WriteLine(InStr(crossWalkArray(0), "$$"))
+        'Console.WriteLine(InStr(crossWalkArray(1), "$$"))
+
+        'Dim arrayPointer As Integer = 0
+        'For i = LBound(crossWalkArray) To UBound(crossWalkArray)
+
+        '    'goodData
+        '    'badData
+        '    'TODO Draw out on board or single step
+        '    Select Case InStr(crossWalkArray(i), "$$") 'return 1 for TRUE 0 for FALSE
+        '        Case 1 'Good Data
+        '            arrayPointer += 1
+        '            crossWalkArray(arrayPointer) = crossWalkArray(i)
+        '        Case 0 ' Bad Data
+        '            crossWalkArray(arrayPointer) = crossWalkArray(i)
+        '    End Select
+
+        'Next
+
+        'UpdateListBox(crossWalkArray)
+
+        UpdateFile(newFileName, crossWalkArray)
+
+    End Sub
+
+
+    '<Remarks>
+    'ReadFile() allows user to choose text file then reads file into TempArray
+    '</Remarks>
+    Private Sub ReadFile(ByVal fileName As String, ByRef recordData() As String)
+        Dim currentRecord As String
+        Dim fileData As String
+        Dim fileNumber As Integer = FreeFile()
+
+        Try
+            FileOpen(fileNumber, fileName, OpenMode.Input)
+            Do While Not EOF(fileNumber)
+                Input(fileNumber, currentRecord)
+                'ListBox1.Items.Add(currentRecord)
+                fileData &= currentRecord
+            Loop
+        Catch ex As Exception
+            'TODO: user select file if it doesn't exist
+            Console.WriteLine(ex.Message)
+        Finally
+            FileClose(fileNumber)
+        End Try
+
+        recordData = Split(fileData, "$$")
+
+    End Sub
+
+    '<Remarks>
+    'Update display data in listbox from TempArray Class level array
+    '</Remarks>
+    Private Sub UpdateListBox(ByVal dataArray() As String)
+        'TODO pass array as argument
+        ListBox1.Items.Clear()
+
+        For i = LBound(dataArray) To UBound(dataArray)
+            ListBox1.Items.Add(dataArray(i))
+        Next
+
+    End Sub
+
+    Private Sub LoadFile()
+
+
+
+
+    End Sub
+
+    Private Sub CreateTempFile()
+        Kill(TempFileName)
+        UpdateFile(TempFileName, TempArray)
+    End Sub
+
+    Private Sub UpdateFile(fileNameToBeUpdated As String, newData() As String)
+        For currentRecord = LBound(newData) To UBound(newData)
+            AppendFile(fileNameToBeUpdated, newData(currentRecord))
+        Next
+    End Sub
+
+    Private Sub AppendFile(ByVal filename As String, newRecord As String)
+        Dim fileNumber As Integer = FreeFile()
+
+        Try
+            FileOpen(fileNumber, filename, OpenMode.Append)
+            Write(fileNumber, newRecord)
+            WriteLine(fileNumber)
+
+        Catch ex As Exception
+            'TODO: user select file if it doesn't exist
+            'handle file in use exception
+            'verify other possible exceptions           
+        Finally
+            FileClose(fileNumber)
+        End Try
+
+    End Sub
+
+    Private Sub WriteFile(ByVal filename As String, newRecord As String)
+        Dim fileNumber As Integer = FreeFile()
+
+        Try
+            FileOpen(fileNumber, filename, OpenMode.Output)
+            Write(fileNumber, newRecord)
+
+        Catch ex As Exception
+            'TODO: user select file if it doesn't exist
+            'handle file in use exception
+            'verify other possible exceptions           
+        Finally
+            FileClose(fileNumber)
+        End Try
+
+    End Sub
+
+    Private Sub WriteTestFile()
+        'Dim myfile As My.Resources
+        'Example from: https://docs.microsoft.com/en-us/dotnet/api/microsoft.visualbasic.filesystem.writeline?view=netframework-4.8
+        ' Open file for output.
+        FileOpen(1, "TestFile.txt", OpenMode.Output)
+        ' Print text to the file. The quotation marks will be in the display.
+        Write(1, "This is a test.")
+        ' Go to the next line.
+        WriteLine(1)
+        ' Skip a line.
+        WriteLine(1)
+        ' Print in two print zones. You will see commas and quotation marks
+        ' in the output file.
+        WriteLine(1, "Zone 1", SPC(10), "Zone 2", "Zone 3", "Zone 4")
+        ' Build a longer string before calling WriteLine.
+        WriteLine(1, "Hello" & "  " & "World")
+        ' Include five leading spaces.
+        WriteLine(1, SPC(5), "Leading spaces")
+        ' Print a word starting at column 10.
+        WriteLine(1, TAB(10), "Hello")
+
+        ' Assign Boolean and Date values.
+        Dim aBool As Boolean
+        Dim aDate As DateTime
+        aBool = False
+        aDate = DateTime.Parse("February 12, 1969")
+
+        ' Dates and Booleans are translated using locale settings of 
+        ' your system.
+        WriteLine(1, aBool & " is a Boolean value.")
+        WriteLine(1, aDate & " is a date.")
+        ' Close the file.
+        FileClose(1)
+
+        ' Contents of TestFile.txt
+        '"This is a test.",
+        '
+        '"Zone 1",          "Zone 2"
+        '"Hello  World"
+        '     "Leading spaces"
+        '         ,"Hello"
+        '"False is a Boolean value."
+        '"2/12/1969 is a date."
+    End Sub
+
+    Private Sub updateTextBoxes(index As Integer)
+        Dim feildData() As String
+        Try
+            feildData = Split(TempArray(index), ",")
+            FirstNameTextBox.Text = feildData(0)
+            LastNameTextBox.Text = feildData(1)
+            CityTextBox.Text = feildData(2)
+            emailTextBox.Text = feildData(3)
+        Catch ex As Exception
+            Console.WriteLine(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub updateRecord(index As Integer)
+        Try
+            TempArray(index) = FirstNameTextBox.Text & "," & LastNameTextBox.Text & "," & CityTextBox.Text & "," & emailTextBox.Text
+        Catch ex As Exception
+
+        End Try
+
+        CreateTempFile()
+    End Sub
+
+    'event handler subs below
+    Private Sub ListBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox1.SelectedIndexChanged
+        'MessageBox.Show("sender: " & sender.ToString & "e: " & Str(e))
+
+        Me.Text = Str(ListBox1.SelectedIndex)
+
+        'FirstNameTextBox.Text = TempArray(ListBox1.SelectedIndex)
+        updateTextBoxes(ListBox1.SelectedIndex)
+    End Sub
+
+    Private Sub ReadFileButton_Click(sender As Object, e As EventArgs) Handles ReadFileButton.Click
+        OpenFileDialog.ShowDialog()
+        ReadFile(OpenFileDialog.FileName, TempArray)
+        UpdateListBox(TempArray)
+    End Sub
+
+    Private Sub OverWriteButton_Click(sender As Object, e As EventArgs) Handles OverWriteButton.Click
+        'WriteFile("TempFile.txt", "Start of file")
+        'ListBoxTesting()
+        ListBox1.Items.Insert(0, "Inserted Data")
+    End Sub
+
+    Private Sub AppendFileButton_Click(sender As Object, e As EventArgs) Handles AppendFileButton.Click
+        'AppendFile("TestFile.txt", "Here is my extra data")
+        'AppendFile("TempFile.txt", "$$" & FirstNameTextBox.Text & "," & LastNameTextBox.Text & "," & CityTextBox.Text & "," & StateTextBox.Text & "," & ZipTextBox.Text)
+        ListBox1.Items.Add("added item" & DateTime.Now.ToString)
+    End Sub
+
+    Private Sub SaveFileButton_Click(sender As Object, e As EventArgs) Handles SaveFileButton.Click
+
+        SaveFileDialog.DefaultExt = ".txt"
+        SaveFileDialog.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*"
+        SaveFileDialog.FileName = "MyFile " & DateTime.Now.Year & DateTime.Now.Month & DateTime.Now.Day
+        'SaveFileDialog.InitialDirectory = System.Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) ' "%Downloads%"
+        SaveFileDialog.ShowDialog()
+        FileCopy(TempFileName, SaveFileDialog.FileName)
+
+    End Sub
+
+    Private Sub UpdateButton_Click(sender As Object, e As EventArgs) Handles UpdateButton.Click
+        updateRecord(ListBox1.SelectedIndex)
+        UpdateListBox(TempArray)
+    End Sub
+
+    Private Sub ExitProgram(sender As Object, e As EventArgs) Handles ExitButton.Click
+        'WriteTestFile()
+        'Me.Close()
+        DataCrossWalk()
+        'ShiftArray()
+    End Sub
+
+
+    'debug stuff below
+    Sub ListBoxTesting()
+
+        ListBox1.Items.Insert(0, "Inserted Data")
+
+    End Sub
+
+    '<Remarks>
+    'Used for debug only
+    '</Remarks>
+    Private Sub testArray(anArray() As String) 'TODO add to array examples
+
+        Dim feildData() As String
+
+        For i = LBound(anArray) To UBound(anArray)
+            Console.WriteLine(anArray(i))
+            ListBox1.Items.Add(anArray(i))
+            feildData = Split(anArray(i), ",")
+            For j = LBound(feildData) To UBound(feildData)
+                Console.WriteLine(feildData(j))
+            Next
+        Next
+    End Sub
+
+
+    Private Sub ShiftArray() 'TODO add to array examples
+        Static myArray(6) As String
+        Dim newData As String = FirstNameTextBox.Text
+        Dim oldData As String
+        Static notFirstRun As Boolean
+
+        If notFirstRun = False Then
+            myArray = {"Fred", "Bill", "Joe", "Jimmy", "Ted", "Frank", "Bob"}
+            notFirstRun = True
+        End If
+
+        For i = UBound(myArray) To LBound(myArray) Step -1
+
+            oldData = myArray(i)
+            myArray(i) = newData
+            newData = oldData
+
+        Next
+
+        UpdateListBox(myArray)
+
+    End Sub
+
+End Class
+
