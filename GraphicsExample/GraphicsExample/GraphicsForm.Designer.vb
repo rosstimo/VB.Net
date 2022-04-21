8dee1f2af249d6e52494ce506955d516ef4349c9
rosstimo <rosstimo@isu.edu>
2022-04-21 08:53:09 -0600

    add flair

--- GraphicsExample/GraphicsExample/GraphicsForm.Designer.vb
+++ GraphicsExample/GraphicsExample/GraphicsForm.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class GraphicsForm
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,7 +20,7 @@ Partial Class GraphicsForm
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.components = New System.ComponentModel.Container()
         Me.ColorDialog1 = New System.Windows.Forms.ColorDialog()
@@ -32,8 +32,13 @@ Partial Class GraphicsForm
         Me.ClearToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.DrawPictureBox = New System.Windows.Forms.PictureBox()
         Me.DrawWavesButton = New System.Windows.Forms.Button()
+        Me.ClearButton = New System.Windows.Forms.Button()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.StatusStrip1 = New System.Windows.Forms.StatusStrip()
+        Me.PositionStatusLabel = New System.Windows.Forms.ToolStripStatusLabel()
         Me.ContextMenuStrip.SuspendLayout()
         CType(Me.DrawPictureBox, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.StatusStrip1.SuspendLayout()
         Me.SuspendLayout()
         '
         'ContextMenuStrip
@@ -62,6 +67,7 @@ Partial Class GraphicsForm
         '
         'SizeToolStripMenuItem
         '
+        Me.SizeToolStripMenuItem.Font = New System.Drawing.Font("Segoe UI", 9.0!)
         Me.SizeToolStripMenuItem.Name = "SizeToolStripMenuItem"
         Me.SizeToolStripMenuItem.Size = New System.Drawing.Size(180, 23)
         '
@@ -76,31 +82,64 @@ Partial Class GraphicsForm
         Me.DrawPictureBox.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
             Or System.Windows.Forms.AnchorStyles.Left) _
             Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-        Me.DrawPictureBox.BackColor = System.Drawing.Color.White
+        Me.DrawPictureBox.BackColor = System.Drawing.Color.Black
         Me.DrawPictureBox.ContextMenuStrip = Me.ContextMenuStrip
         Me.DrawPictureBox.Cursor = System.Windows.Forms.Cursors.Cross
         Me.DrawPictureBox.Location = New System.Drawing.Point(12, 12)
-        Me.DrawPictureBox.MinimumSize = New System.Drawing.Size(360, 201)
+        Me.DrawPictureBox.MinimumSize = New System.Drawing.Size(361, 201)
         Me.DrawPictureBox.Name = "DrawPictureBox"
-        Me.DrawPictureBox.Size = New System.Drawing.Size(360, 212)
+        Me.DrawPictureBox.Size = New System.Drawing.Size(361, 201)
         Me.DrawPictureBox.TabIndex = 1
         Me.DrawPictureBox.TabStop = False
         '
         'DrawWavesButton
         '
-        Me.DrawWavesButton.Location = New System.Drawing.Point(12, 231)
+        Me.DrawWavesButton.Anchor = System.Windows.Forms.AnchorStyles.Left
+        Me.DrawWavesButton.Location = New System.Drawing.Point(12, 219)
         Me.DrawWavesButton.Name = "DrawWavesButton"
         Me.DrawWavesButton.Size = New System.Drawing.Size(162, 64)
         Me.DrawWavesButton.TabIndex = 2
         Me.DrawWavesButton.Text = "Draw &Waves"
         Me.DrawWavesButton.UseVisualStyleBackColor = True
         '
+        'ClearButton
+        '
+        Me.ClearButton.Anchor = System.Windows.Forms.AnchorStyles.Left
+        Me.ClearButton.Location = New System.Drawing.Point(211, 219)
+        Me.ClearButton.Name = "ClearButton"
+        Me.ClearButton.Size = New System.Drawing.Size(162, 64)
+        Me.ClearButton.TabIndex = 3
+        Me.ClearButton.Text = "&Clear"
+        Me.ClearButton.UseVisualStyleBackColor = True
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 200
+        '
+        'StatusStrip1
+        '
+        Me.StatusStrip1.ImageScalingSize = New System.Drawing.Size(24, 24)
+        Me.StatusStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.PositionStatusLabel})
+        Me.StatusStrip1.Location = New System.Drawing.Point(0, 306)
+        Me.StatusStrip1.Name = "StatusStrip1"
+        Me.StatusStrip1.Size = New System.Drawing.Size(394, 22)
+        Me.StatusStrip1.TabIndex = 4
+        Me.StatusStrip1.Text = "StatusStrip1"
+        '
+        'PositionStatusLabel
+        '
+        Me.PositionStatusLabel.Name = "PositionStatusLabel"
+        Me.PositionStatusLabel.Size = New System.Drawing.Size(50, 17)
+        Me.PositionStatusLabel.Text = "position"
+        '
         'GraphicsForm
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.SystemColors.Control
-        Me.ClientSize = New System.Drawing.Size(394, 307)
+        Me.ClientSize = New System.Drawing.Size(394, 328)
+        Me.Controls.Add(Me.StatusStrip1)
+        Me.Controls.Add(Me.ClearButton)
         Me.Controls.Add(Me.DrawWavesButton)
         Me.Controls.Add(Me.DrawPictureBox)
         Me.MinimumSize = New System.Drawing.Size(410, 335)
@@ -110,7 +149,10 @@ Partial Class GraphicsForm
         Me.ContextMenuStrip.ResumeLayout(False)
         Me.ContextMenuStrip.PerformLayout()
         CType(Me.DrawPictureBox, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.StatusStrip1.ResumeLayout(False)
+        Me.StatusStrip1.PerformLayout()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
@@ -123,4 +165,8 @@ Partial Class GraphicsForm
     Friend WithEvents ToolStripSeparator1 As ToolStripSeparator
     Friend WithEvents ToolStripMenuItem1 As ToolStripMenuItem
     Friend WithEvents DrawWavesButton As Button
+    Friend WithEvents ClearButton As Button
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents StatusStrip1 As StatusStrip
+    Friend WithEvents PositionStatusLabel As ToolStripStatusLabel
 End Class
--- GraphicsExample/GraphicsExample/GraphicsForm.vb
+++ GraphicsExample/GraphicsExample/GraphicsForm.vb
@@ -20,14 +20,35 @@ Public Class GraphicsForm
 
     End Sub
 
+    Sub Shake()
+        Dim offset As Integer = 50
+        ShakeSound()
+        For i = 1 To 12
+            System.Threading.Thread.Sleep(200) 'wait 200ms
+            Me.Left += offset
+            Me.Top += offset
+            offset *= -1 'invert offset
+        Next
+    End Sub
+
+    Private Sub ShakeSound()
+        'https://freesound.org/
+        Try
+            My.Computer.Audio.Play(My.Resources.shaker, AudioPlayMode.Background)
+        Catch ex As Exception
+        End Try
+    End Sub
+
     Sub PickPenColor()
         ColorDialog1.ShowDialog()
         Me.currentColor = ColorDialog1.Color
-
+        UpdateSTatusBar()
     End Sub
 
     Sub DrawSinWave()
         Dim x, y, ymax, oldY, oldX As Integer
+        Dim oldColor As Color = Me.currentColor
+        Me.currentColor = Color.LightGreen
         ymax = 100
         x = 45
         oldY = ymax
@@ -48,11 +69,14 @@ Public Class GraphicsForm
             oldY = y
 
         Next
+        Me.currentColor = oldColor
 
     End Sub
 
     Sub DrawCosWave()
         Dim x, y, ymax, oldY, oldX As Integer
+        Dim oldColor As Color = Me.currentColor
+        Me.currentColor = Color.LightBlue
         ymax = 100
         x = 45
         oldY = ymax
@@ -70,35 +94,65 @@ Public Class GraphicsForm
             oldY = y
 
         Next
+        Me.currentColor = oldColor
 
     End Sub
 
     Sub DrawTanWave()
         Dim x, y, ymax, oldY, oldX As Integer
+        Dim oldColor As Color = Me.currentColor
+        Me.currentColor = Color.Red
         ymax = 100
         x = 45
         oldY = ymax
-        For x = 0 To 360 Step 1
 
+        For x = 0 To 360 Step 1
             'use system.math
             'degrees must be converted to radians deg * (PI/180)
 
             'Draw tan
-            y = CInt(ymax * Tan(x * (PI / 180)) * -1) + ymax
-
-
-            Sketch(oldX, oldY, x, y)
-            'Console.WriteLine($"({x},{y})")
-            oldX = x
-            oldY = y
+            Try
+                'don't crash on +- infinite
+                y = CInt(ymax * Tan(x * (PI / 180)) * -1) + ymax
+                Sketch(oldX, oldY, x, y)
+                oldY = y
+                oldX = x
+            Catch ex As Exception
+                Console.WriteLine($"oops infinite is too hard to handle!")
+                Console.WriteLine(ex.Message)
+            End Try
 
         Next
+        Me.currentColor = oldColor
 
     End Sub
 
+    Sub DrawDivisions()
+        Dim verticalDivisions As Integer = 10
+        Dim horizontalDivisions As Integer = 8
+        Dim oldColor As Color = Me.currentColor
 
+        Me.currentColor = Color.Gray
 
+        'Draw division lines top to bottom
+        For v = 0 To 360 Step DrawPictureBox.Width \ verticalDivisions
+            Sketch(v, 0, v, DrawPictureBox.Height)
+        Next
 
+        'Draw division lines left to right
+        For h = 0 To 200 Step DrawPictureBox.Height \ horizontalDivisions
+            Sketch(0, h, DrawPictureBox.Width, h)
+        Next
+
+        Me.currentColor = oldColor
+    End Sub
+
+    Sub UpdateSTatusBar(Optional x As Integer = 0, Optional y As Integer = 0)
+
+        PositionStatusLabel.Text = currentColor.Name & "   " &
+        CStr(currentPenSize) & " px   " & _ 'TODO update pixels,cm,inch etc.
+        $"({x},{y})".PadLeft(15)
+    End Sub
 
     Private Sub GraphicsForm_MouseMove(sender As Object, e As MouseEventArgs) _
         Handles DrawPictureBox.MouseMove, DrawPictureBox.MouseDown
@@ -115,16 +169,18 @@ Public Class GraphicsForm
 
         oldX = e.X
         oldY = e.Y
-
+        UpdateSTatusBar(e.X, e.Y)
     End Sub
 
     Private Sub GraphicsForm_Load(sender As Object, e As EventArgs) Handles Me.Load
         currentColor = Color.Black
         currentPenSize = 1
+        UpdateSTatusBar()
     End Sub
 
     Private Sub ClearToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ClearToolStripMenuItem.Click
         Clear()
+
     End Sub
 
     Private Sub ColorToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ColorToolStripMenuItem.Click
@@ -138,10 +194,20 @@ Public Class GraphicsForm
         Catch ex As Exception
 
         End Try
+
     End Sub
 
     Private Sub DrawWavesButton_Click(sender As Object, e As EventArgs) Handles DrawWavesButton.Click
+        Clear()
+        DrawDivisions()
         DrawSinWave()
+        DrawCosWave()
+        DrawTanWave()
+    End Sub
+
+    Private Sub ClearButton_Click(sender As Object, e As EventArgs) Handles ClearButton.Click
+        Shake()
+        Clear()
     End Sub
 End Class
 
