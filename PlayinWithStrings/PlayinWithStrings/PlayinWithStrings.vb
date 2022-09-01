a011616c890015608428861517d6349756446e0a
rosstimo <rosstimo@isu.edu>
2022-09-01 08:15:43 -0600

    reorganize strings example

--- PlayinWithStrings/PlayinWithStrings/PlayinWithStrings.vb
+++ PlayinWithStrings/PlayinWithStrings/PlayinWithStrings.vb
@@ -10,33 +10,33 @@ Module PlayinWithStrings
 
     Sub Main()
         Dim testString As String = "Hello, World!!"
-        Dim money As Decimal = 1426.876273897289@
-        Dim length As Integer
-        Dim temp() As String
+        'Dim money As Decimal = 1426.876273897289@
+        'Dim length As Integer
+        'Dim temp() As String
 
+        Console.WriteLine(StrReverse(testString))
+        Console.WriteLine(UCase(testString))
+        Console.WriteLine(LCase(testString))
+        testString = Replace(testString, "W", "X")
 
-        temp = Split(testString, ", ")
-        Console.WriteLine(temp(1))
 
         'testString = Left(testString, 7)
         'testString = Right(testString, 2)
         'testString = Mid(testString, 5)
         'testString = Left(testString, 4)
 
+        'Console.WriteLine(testString)
+        'Console.WriteLine(money.ToString("C"))
 
         'For i = Len(testString) - 1 To 0 Step -1
         '    Console.Write(testString(i))
         'Next
         'Console.WriteLine()
 
-        'Console.WriteLine(StrReverse(testString))
-        'Console.WriteLine(UCase(testString))
-        'Console.WriteLine(LCase(testString))
-        'testString = Replace(testString, "W", "X")
+        'temp = Split(testString, ", ")
+        'Console.WriteLine(temp(1))
 
 
-        'Console.WriteLine(testString)
-        'Console.WriteLine(money.ToString("C"))
 
         Console.Read()
     End Sub
