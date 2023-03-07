e01a8e1cf9575f25311fdb2e7765339ed14d5a39
tim <rosstimo@isu.edu>
2023-03-07 07:57:05 -0700

    add examples

--- ListsAndListBoxExample/ListsAndListBoxExample/ListsAndListBoxExampleForm.Designer.vb
+++ ListsAndListBoxExample/ListsAndListBoxExample/ListsAndListBoxExampleForm.Designer.vb
@@ -46,10 +46,13 @@ Partial Class ListsAndListBoxExampleForm
         '
         Me.DisplayListBox.ContextMenuStrip = Me.ContextMenuStrip
         Me.DisplayListBox.Cursor = System.Windows.Forms.Cursors.IBeam
+        Me.DisplayListBox.Font = New System.Drawing.Font("Consolas", 8.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.DisplayListBox.FormattingEnabled = True
-        Me.DisplayListBox.Location = New System.Drawing.Point(12, 51)
+        Me.DisplayListBox.ItemHeight = 19
+        Me.DisplayListBox.Location = New System.Drawing.Point(18, 78)
+        Me.DisplayListBox.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.DisplayListBox.Name = "DisplayListBox"
-        Me.DisplayListBox.Size = New System.Drawing.Size(776, 251)
+        Me.DisplayListBox.Size = New System.Drawing.Size(1162, 384)
         Me.DisplayListBox.TabIndex = 0
         Me.MainToolTip.SetToolTip(Me.DisplayListBox, "Select an item then right click for more options.")
         '
@@ -58,33 +61,35 @@ Partial Class ListsAndListBoxExampleForm
         Me.ContextMenuStrip.ImageScalingSize = New System.Drawing.Size(24, 24)
         Me.ContextMenuStrip.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.RemoveContextMenuItem, Me.ClearAllContextMenuItem})
         Me.ContextMenuStrip.Name = "ContextMenuStrip"
-        Me.ContextMenuStrip.Size = New System.Drawing.Size(119, 48)
+        Me.ContextMenuStrip.Size = New System.Drawing.Size(149, 68)
         '
         'RemoveContextMenuItem
         '
         Me.RemoveContextMenuItem.Name = "RemoveContextMenuItem"
-        Me.RemoveContextMenuItem.Size = New System.Drawing.Size(118, 22)
+        Me.RemoveContextMenuItem.Size = New System.Drawing.Size(148, 32)
         Me.RemoveContextMenuItem.Text = "Remove"
         '
         'ClearAllContextMenuItem
         '
         Me.ClearAllContextMenuItem.Name = "ClearAllContextMenuItem"
-        Me.ClearAllContextMenuItem.Size = New System.Drawing.Size(118, 22)
+        Me.ClearAllContextMenuItem.Size = New System.Drawing.Size(148, 32)
         Me.ClearAllContextMenuItem.Text = "Clear All"
         '
         'ItemsComboBox
         '
         Me.ItemsComboBox.FormattingEnabled = True
-        Me.ItemsComboBox.Location = New System.Drawing.Point(541, 308)
+        Me.ItemsComboBox.Location = New System.Drawing.Point(812, 474)
+        Me.ItemsComboBox.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.ItemsComboBox.Name = "ItemsComboBox"
-        Me.ItemsComboBox.Size = New System.Drawing.Size(247, 21)
+        Me.ItemsComboBox.Size = New System.Drawing.Size(368, 28)
         Me.ItemsComboBox.TabIndex = 1
         '
         'QuitButton
         '
-        Me.QuitButton.Location = New System.Drawing.Point(640, 374)
+        Me.QuitButton.Location = New System.Drawing.Point(960, 575)
+        Me.QuitButton.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.QuitButton.Name = "QuitButton"
-        Me.QuitButton.Size = New System.Drawing.Size(148, 64)
+        Me.QuitButton.Size = New System.Drawing.Size(222, 98)
         Me.QuitButton.TabIndex = 2
         Me.QuitButton.Text = "&Quit"
         Me.MainToolTip.SetToolTip(Me.QuitButton, "Click to leave")
@@ -92,9 +97,10 @@ Partial Class ListsAndListBoxExampleForm
         '
         'AddButton
         '
-        Me.AddButton.Location = New System.Drawing.Point(486, 374)
+        Me.AddButton.Location = New System.Drawing.Point(729, 575)
+        Me.AddButton.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.AddButton.Name = "AddButton"
-        Me.AddButton.Size = New System.Drawing.Size(148, 64)
+        Me.AddButton.Size = New System.Drawing.Size(222, 98)
         Me.AddButton.TabIndex = 3
         Me.AddButton.Text = "&Add"
         Me.MainToolTip.SetToolTip(Me.AddButton, "Add From Text Box")
@@ -102,18 +108,20 @@ Partial Class ListsAndListBoxExampleForm
         '
         'TextBox1
         '
-        Me.TextBox1.Location = New System.Drawing.Point(12, 308)
+        Me.TextBox1.Location = New System.Drawing.Point(18, 474)
+        Me.TextBox1.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(216, 20)
+        Me.TextBox1.Size = New System.Drawing.Size(322, 26)
         Me.TextBox1.TabIndex = 4
         '
         'TopMenuStrip
         '
+        Me.TopMenuStrip.GripMargin = New System.Windows.Forms.Padding(2, 2, 0, 2)
         Me.TopMenuStrip.ImageScalingSize = New System.Drawing.Size(24, 24)
         Me.TopMenuStrip.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem, Me.HelpToolStripMenuItem})
         Me.TopMenuStrip.Location = New System.Drawing.Point(0, 0)
         Me.TopMenuStrip.Name = "TopMenuStrip"
-        Me.TopMenuStrip.Size = New System.Drawing.Size(800, 24)
+        Me.TopMenuStrip.Size = New System.Drawing.Size(1200, 35)
         Me.TopMenuStrip.TabIndex = 5
         Me.TopMenuStrip.Text = "MenuStrip1"
         Me.MainToolTip.SetToolTip(Me.TopMenuStrip, "Select an item then right click for more options.")
@@ -122,39 +130,39 @@ Partial Class ListsAndListBoxExampleForm
         '
         Me.FileToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.AddToolStripMenuItem, Me.CloseToolStripMenuItem})
         Me.FileToolStripMenuItem.Name = "FileToolStripMenuItem"
-        Me.FileToolStripMenuItem.Size = New System.Drawing.Size(37, 20)
+        Me.FileToolStripMenuItem.Size = New System.Drawing.Size(54, 30)
         Me.FileToolStripMenuItem.Text = "&File"
         '
         'AddToolStripMenuItem
         '
         Me.AddToolStripMenuItem.Name = "AddToolStripMenuItem"
-        Me.AddToolStripMenuItem.Size = New System.Drawing.Size(103, 22)
+        Me.AddToolStripMenuItem.Size = New System.Drawing.Size(157, 34)
         Me.AddToolStripMenuItem.Text = "Add"
         '
         'CloseToolStripMenuItem
         '
         Me.CloseToolStripMenuItem.Name = "CloseToolStripMenuItem"
-        Me.CloseToolStripMenuItem.Size = New System.Drawing.Size(103, 22)
+        Me.CloseToolStripMenuItem.Size = New System.Drawing.Size(157, 34)
         Me.CloseToolStripMenuItem.Text = "Close"
         '
         'HelpToolStripMenuItem
         '
         Me.HelpToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.AboutToolStripMenuItem})
         Me.HelpToolStripMenuItem.Name = "HelpToolStripMenuItem"
-        Me.HelpToolStripMenuItem.Size = New System.Drawing.Size(44, 20)
+        Me.HelpToolStripMenuItem.Size = New System.Drawing.Size(65, 30)
         Me.HelpToolStripMenuItem.Text = "&Help"
         '
         'AboutToolStripMenuItem
         '
         Me.AboutToolStripMenuItem.Name = "AboutToolStripMenuItem"
-        Me.AboutToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.AboutToolStripMenuItem.Size = New System.Drawing.Size(164, 34)
         Me.AboutToolStripMenuItem.Text = "&About"
         '
         'ListsAndListBoxExampleForm
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(1200, 692)
         Me.Controls.Add(Me.TextBox1)
         Me.Controls.Add(Me.AddButton)
         Me.Controls.Add(Me.QuitButton)
@@ -162,6 +170,7 @@ Partial Class ListsAndListBoxExampleForm
         Me.Controls.Add(Me.DisplayListBox)
         Me.Controls.Add(Me.TopMenuStrip)
         Me.MainMenuStrip = Me.TopMenuStrip
+        Me.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.Name = "ListsAndListBoxExampleForm"
         Me.Text = "List Controls"
         Me.ContextMenuStrip.ResumeLayout(False)
