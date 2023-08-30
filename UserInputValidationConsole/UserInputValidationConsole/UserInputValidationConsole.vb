62be90a7cd0518dc02a5c7fc5b3da4bb1f042fd1
tim <rosstimo@isu.edu>
2023-08-30 08:44:31 -0600

    add get user input and attempt to convert

--- UserInputValidationConsole/UserInputValidationConsole/UserInputValidationConsole.vb
+++ UserInputValidationConsole/UserInputValidationConsole/UserInputValidationConsole.vb
@@ -11,6 +11,22 @@
         'Continue until the user enters a valid number or enter "q"
         'Check if the number is within the range
         'If yes congratulate the user and start over
+        Dim userInput As String
+        Dim userNumber As Integer
+
+        Console.WriteLine("Please enter a number from 1 to 10")
+        Console.WriteLine("Enter 'q' to quit")
+
+        'get user input
+        userInput = Console.ReadLine()
+        'reflect the user input back to the user
+        Console.WriteLine($"You entered: {userInput}")
+
+        'attempt to convert the user input to an integer
+        userNumber = CInt(userInput)
+
+        'pause here so the user can read the console before exit
+        Console.Read()
 
     End Sub
 
