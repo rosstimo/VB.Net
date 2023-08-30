909f69baea667f663e450349ef84a0d83944ea94
tim <rosstimo@isu.edu>
2023-08-30 08:34:15 -0600

    add TODO

--- UserInputValidationConsole/UserInputValidationConsole/UserInputValidationConsole.vb
+++ UserInputValidationConsole/UserInputValidationConsole/UserInputValidationConsole.vb
@@ -1,6 +1,16 @@
 ﻿Module UserInputValidationConsole
 
     Sub Main()
+        'TODO
+        'Prompt the user for a number from 1 to 10 inclusive or "q" to quit
+        'Reflect the user input back to the user
+        'Validate the user input
+        'Check if the input is a number
+        'If the user entered "q" or "Q" then quit the program
+        'If not, display an error message and prompt the user again
+        'Continue until the user enters a valid number or enter "q"
+        'Check if the number is within the range
+        'If yes congratulate the user and start over
 
     End Sub
 
