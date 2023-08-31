e0539579931f90348fd451644b7a9363d7e8fff9
tim <rosstimo@isu.edu>
2023-08-31 08:21:45 -0600

    add q to quit evaluation

--- UserInputValidationConsole/UserInputValidationConsole/UserInputValidationConsole.vb
+++ UserInputValidationConsole/UserInputValidationConsole/UserInputValidationConsole.vb
@@ -1,16 +1,20 @@
-﻿Module UserInputValidationConsole
+﻿Option Strict On
+Option Explicit On
+Option Compare Text
+
+Module UserInputValidationConsole
 
     Sub Main()
         'TODO
-        'Prompt the user for a number from 1 to 10 inclusive or "q" to quit
-        'Reflect the user input back to the user
-        'Validate the user input
-        'Check if the input is a number
-        'If the user entered "q" or "Q" then quit the program
-        'If not, display an error message and prompt the user again
-        'Continue until the user enters a valid number or enter "q"
-        'Check if the number is within the range
-        'If yes congratulate the user and start over
+        '[ ] Prompt the user for a number from 1 to 10 inclusive or "q" to quit
+        '[*] Reflect the user input back to the user
+        '[ ] Validate the user input
+        '[*] Check if the input is a number
+        '[*] If the user entered "q" or "Q" then quit the program
+        '[*] If not, display an error message and prompt the user again
+        '[ ] Continue until the user enters a valid number or enter "q"
+        '[ ] Check if the number is within the range
+        '[ ] If yes congratulate the user and start over
         Dim userInput As String
         Dim userNumber As Integer
         Dim exitFlag As Boolean = False
@@ -28,10 +32,14 @@
             Try
                 userNumber = CInt(userInput)
             Catch ex As Exception
-                Console.WriteLine($"I'm sorry {userInput} is not a number")
+                If userInput = "q" Then 'Option Compare Text is set so we don't have to worry about case
+                    exitFlag = True
+                Else
+                    Console.WriteLine($"I'm sorry {userInput} is not a number")
+                End If
             End Try
-        Loop Until exitflag = True
-
+        Loop Until exitFlag = True
+        Console.WriteLine("Have a nice day!")
         'pause here so the user can read the console before exit
         Console.Read()
     End Sub
