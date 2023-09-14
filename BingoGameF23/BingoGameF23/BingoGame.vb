37cb9bc03f6914bb0e0a71de72323ee81d4c6474
tim <rosstimo@isu.edu>
2023-09-14 08:22:55 -0600

    add TODO

--- BingoGameF23/BingoGameF23/BingoGame.vb
+++ BingoGameF23/BingoGameF23/BingoGame.vb
@@ -1,4 +1,22 @@
-﻿Module BingoGame
+﻿'Bingo Game
+'Fall 2023
+'In class examples and practice
+'
+
+
+'TODO
+'[ ] Create an array to track the numbers called
+'[ ] Create a function to display the balls already called
+'[ ] Create a way to draw a ball, check if it has been called, and track it in the array
+'[ ] Create a way to restart. should happen automatically if all balls are called
+'[ ] new game, play again or quit functionality
+'Later generate player cards and make playable 
+'[ ] Create a way to display the bingo card
+
+Option Explicit On
+Option Strict On
+
+Module BingoGame
 
     Sub Main()
 
