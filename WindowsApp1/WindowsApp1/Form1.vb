6d463e616733d5453c2db64ff3181596d1e4d834
tim <rosstimo@isu.edu>
2023-10-25 08:17:44 -0600

    init

---
+++ WindowsApp1/WindowsApp1/Form1.vb
@@ -0,0 +1,5 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Button1.BackColor = Color.FromArgb(164, 164, 164)
+    End Sub
+End Class
