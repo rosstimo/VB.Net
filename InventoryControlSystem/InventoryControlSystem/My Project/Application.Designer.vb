b3459f5a57718968e793fd44b958e68b4750ee31
tim <rosstimo@isu.edu>
2023-10-25 08:21:11 -0600

    Fix rename error

--- InventoryControlSystem/InventoryControlSystem/My Project/Application.Designer.vb
+++ InventoryControlSystem/InventoryControlSystem/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
 
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()> _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.InventoryControlSystem.Form1
+            Me.MainForm = Global.InventoryControlSystem.InventoryControlForm
         End Sub
     End Class
 End Namespace
