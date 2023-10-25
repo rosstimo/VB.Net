7980c170717fb062ff7b87f17bfdbd0de6ba788c
tim <rosstimo@isu.edu>
2023-10-25 08:47:44 -0600

    add controls, group boxes, set tab order

--- InventoryControlSystem/InventoryControlSystem/InventoryControlForm.Designer.vb
+++ InventoryControlSystem/InventoryControlSystem/InventoryControlForm.Designer.vb
@@ -22,10 +22,237 @@ Partial Class InventoryControlForm
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.TextBox6 = New System.Windows.Forms.TextBox()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox2 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox3 = New System.Windows.Forms.CheckBox()
+        Me.RadioButton4 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton5 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton6 = New System.Windows.Forms.RadioButton()
+        Me.ButtonsGroupBox = New System.Windows.Forms.GroupBox()
+        Me.OptionsGroupBox = New System.Windows.Forms.GroupBox()
+        Me.RecordInfoGroupBox = New System.Windows.Forms.GroupBox()
+        Me.ButtonsGroupBox.SuspendLayout()
+        Me.OptionsGroupBox.SuspendLayout()
+        Me.RecordInfoGroupBox.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(278, 25)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(130, 51)
+        Me.Button1.TabIndex = 12
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(142, 25)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(130, 51)
+        Me.Button2.TabIndex = 11
+        Me.Button2.Text = "Button2"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(6, 25)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(187, 26)
+        Me.TextBox1.TabIndex = 0
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(6, 57)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(187, 26)
+        Me.TextBox2.TabIndex = 1
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(6, 25)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(130, 51)
+        Me.Button3.TabIndex = 10
+        Me.Button3.Text = "Button3"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(6, 121)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(187, 26)
+        Me.TextBox3.TabIndex = 3
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(6, 89)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(187, 26)
+        Me.TextBox4.TabIndex = 2
+        '
+        'TextBox5
+        '
+        Me.TextBox5.Location = New System.Drawing.Point(6, 185)
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(187, 26)
+        Me.TextBox5.TabIndex = 5
+        '
+        'TextBox6
+        '
+        Me.TextBox6.Location = New System.Drawing.Point(6, 153)
+        Me.TextBox6.Name = "TextBox6"
+        Me.TextBox6.Size = New System.Drawing.Size(187, 26)
+        Me.TextBox6.TabIndex = 4
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(6, 25)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(116, 24)
+        Me.CheckBox1.TabIndex = 0
+        Me.CheckBox1.Text = "CheckBox1"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'CheckBox2
+        '
+        Me.CheckBox2.AutoSize = True
+        Me.CheckBox2.Location = New System.Drawing.Point(6, 56)
+        Me.CheckBox2.Name = "CheckBox2"
+        Me.CheckBox2.Size = New System.Drawing.Size(116, 24)
+        Me.CheckBox2.TabIndex = 1
+        Me.CheckBox2.Text = "CheckBox2"
+        Me.CheckBox2.UseVisualStyleBackColor = True
+        '
+        'CheckBox3
+        '
+        Me.CheckBox3.AutoSize = True
+        Me.CheckBox3.Location = New System.Drawing.Point(6, 89)
+        Me.CheckBox3.Name = "CheckBox3"
+        Me.CheckBox3.Size = New System.Drawing.Size(116, 24)
+        Me.CheckBox3.TabIndex = 2
+        Me.CheckBox3.Text = "CheckBox3"
+        Me.CheckBox3.UseVisualStyleBackColor = True
+        '
+        'RadioButton4
+        '
+        Me.RadioButton4.AutoSize = True
+        Me.RadioButton4.Location = New System.Drawing.Point(6, 119)
+        Me.RadioButton4.Name = "RadioButton4"
+        Me.RadioButton4.Size = New System.Drawing.Size(133, 24)
+        Me.RadioButton4.TabIndex = 3
+        Me.RadioButton4.TabStop = True
+        Me.RadioButton4.Text = "RadioButton4"
+        Me.RadioButton4.UseVisualStyleBackColor = True
+        '
+        'RadioButton5
+        '
+        Me.RadioButton5.AutoSize = True
+        Me.RadioButton5.Location = New System.Drawing.Point(6, 149)
+        Me.RadioButton5.Name = "RadioButton5"
+        Me.RadioButton5.Size = New System.Drawing.Size(133, 24)
+        Me.RadioButton5.TabIndex = 3
+        Me.RadioButton5.TabStop = True
+        Me.RadioButton5.Text = "RadioButton5"
+        Me.RadioButton5.UseVisualStyleBackColor = True
+        '
+        'RadioButton6
+        '
+        Me.RadioButton6.AutoSize = True
+        Me.RadioButton6.Location = New System.Drawing.Point(6, 179)
+        Me.RadioButton6.Name = "RadioButton6"
+        Me.RadioButton6.Size = New System.Drawing.Size(133, 24)
+        Me.RadioButton6.TabIndex = 3
+        Me.RadioButton6.TabStop = True
+        Me.RadioButton6.Text = "RadioButton6"
+        Me.RadioButton6.UseVisualStyleBackColor = True
+        '
+        'ButtonsGroupBox
+        '
+        Me.ButtonsGroupBox.Controls.Add(Me.Button3)
+        Me.ButtonsGroupBox.Controls.Add(Me.Button2)
+        Me.ButtonsGroupBox.Controls.Add(Me.Button1)
+        Me.ButtonsGroupBox.Location = New System.Drawing.Point(372, 341)
+        Me.ButtonsGroupBox.Name = "ButtonsGroupBox"
+        Me.ButtonsGroupBox.Size = New System.Drawing.Size(418, 97)
+        Me.ButtonsGroupBox.TabIndex = 2
+        Me.ButtonsGroupBox.TabStop = False
+        '
+        'OptionsGroupBox
+        '
+        Me.OptionsGroupBox.Controls.Add(Me.CheckBox1)
+        Me.OptionsGroupBox.Controls.Add(Me.CheckBox2)
+        Me.OptionsGroupBox.Controls.Add(Me.RadioButton6)
+        Me.OptionsGroupBox.Controls.Add(Me.CheckBox3)
+        Me.OptionsGroupBox.Controls.Add(Me.RadioButton5)
+        Me.OptionsGroupBox.Controls.Add(Me.RadioButton4)
+        Me.OptionsGroupBox.Location = New System.Drawing.Point(372, 12)
+        Me.OptionsGroupBox.Name = "OptionsGroupBox"
+        Me.OptionsGroupBox.Size = New System.Drawing.Size(416, 323)
+        Me.OptionsGroupBox.TabIndex = 1
+        Me.OptionsGroupBox.TabStop = False
+        Me.OptionsGroupBox.Text = "Options"
+        '
+        'RecordInfoGroupBox
+        '
+        Me.RecordInfoGroupBox.Controls.Add(Me.TextBox1)
+        Me.RecordInfoGroupBox.Controls.Add(Me.TextBox2)
+        Me.RecordInfoGroupBox.Controls.Add(Me.TextBox4)
+        Me.RecordInfoGroupBox.Controls.Add(Me.TextBox3)
+        Me.RecordInfoGroupBox.Controls.Add(Me.TextBox6)
+        Me.RecordInfoGroupBox.Controls.Add(Me.TextBox5)
+        Me.RecordInfoGroupBox.Location = New System.Drawing.Point(12, 12)
+        Me.RecordInfoGroupBox.Name = "RecordInfoGroupBox"
+        Me.RecordInfoGroupBox.Size = New System.Drawing.Size(338, 323)
+        Me.RecordInfoGroupBox.TabIndex = 0
+        Me.RecordInfoGroupBox.TabStop = False
+        Me.RecordInfoGroupBox.Text = "Record Info"
+        '
+        'InventoryControlForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.RecordInfoGroupBox)
+        Me.Controls.Add(Me.OptionsGroupBox)
+        Me.Controls.Add(Me.ButtonsGroupBox)
+        Me.Name = "InventoryControlForm"
         Me.Text = "Form1"
+        Me.ButtonsGroupBox.ResumeLayout(False)
+        Me.OptionsGroupBox.ResumeLayout(False)
+        Me.OptionsGroupBox.PerformLayout()
+        Me.RecordInfoGroupBox.ResumeLayout(False)
+        Me.RecordInfoGroupBox.PerformLayout()
+        Me.ResumeLayout(False)
+
     End Sub
 
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Button3 As Button
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents TextBox6 As TextBox
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents CheckBox2 As CheckBox
+    Friend WithEvents CheckBox3 As CheckBox
+    Friend WithEvents RadioButton4 As RadioButton
+    Friend WithEvents RadioButton5 As RadioButton
+    Friend WithEvents RadioButton6 As RadioButton
+    Friend WithEvents ButtonsGroupBox As GroupBox
+    Friend WithEvents OptionsGroupBox As GroupBox
+    Friend WithEvents RecordInfoGroupBox As GroupBox
 End Class
--- InventoryControlSystem/InventoryControlSystem/InventoryControlForm.vb
+++ InventoryControlSystem/InventoryControlSystem/InventoryControlForm.vb
@@ -1,3 +1,11 @@
 ﻿Public Class InventoryControlForm
+    Sub SetDefaults()
+        RadioButton4.Checked = True
+    End Sub
+
+    ' Event Handlers Below
+    Private Sub InventoryControlForm_Load(sender As Object, e As EventArgs) Handles Me.Load
+        SetDefaults()
+    End Sub
 
 End Class
