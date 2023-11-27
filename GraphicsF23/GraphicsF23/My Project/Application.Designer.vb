f7ac03fdb46b0a5f526c78952d0b9ed279b05fa1
tim <rosstimo@isu.edu>
2023-11-27 08:29:18 -0700

    fix rename error

--- GraphicsF23/GraphicsF23/My Project/Application.Designer.vb
+++ GraphicsF23/GraphicsF23/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
 
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()> _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.GraphicsF23.Form1
+            Me.MainForm = Global.GraphicsF23.GraphicsForm
         End Sub
     End Class
 End Namespace
