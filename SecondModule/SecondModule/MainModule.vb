c48ebcf950d93756f0ce1880124c6df198234b2d
rosstimo <rosstimo@isu.edu>
2024-01-30 08:14:50 -0700

    initial code

--- SecondModule/SecondModule/MainModule.vb
+++ SecondModule/SecondModule/MainModule.vb
@@ -1,7 +1,13 @@
 ﻿Module MainModule
 
     Sub Main()
+        SayHello()
+        Console.Read()
+    End Sub
 
+
+    Sub SayHello()
+        Console.WriteLine("Hello!")
     End Sub
 
 End Module
