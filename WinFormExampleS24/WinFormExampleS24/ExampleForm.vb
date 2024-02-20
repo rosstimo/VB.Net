86a9311d5ed9581c7d3fd3de4e83a505e5965503
rosstimo <rosstimo@isu.edu>
2024-02-20 08:36:52 -0700

    add event handlers

--- WinFormExampleS24/WinFormExampleS24/ExampleForm.vb
+++ WinFormExampleS24/WinFormExampleS24/ExampleForm.vb
@@ -1,3 +1,18 @@
-﻿Public Class ExampleForm
+﻿'header stuff
+
+Option Strict On
+Option Explicit On
+
+
+Public Class ExampleForm
+
+
+    Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
+        Me.Close()
+    End Sub
+
+    Private Sub SubmitButton_Click(sender As Object, e As EventArgs) Handles SubmitButton.Click
+        UserInputTextBox.Text = StrReverse(UserInputTextBox.Text)
+    End Sub
 
 End Class
