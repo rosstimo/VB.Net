5380c79894934ee938529b4bd44e11028b8f5c13
rosstimo <rosstimo@isu.edu>
2024-08-22 08:40:03 -0600

    add color and sound example

--- ProgramFlowExamples/ClassRoomExamples/ClassRoomExamples/ClassRoomExamples.vb
+++ ProgramFlowExamples/ClassRoomExamples/ClassRoomExamples/ClassRoomExamples.vb
@@ -5,6 +5,25 @@ Module ClassRoomExamples
 
     Sub Main()
 
+        'change entire background
+        Console.BackgroundColor() = ConsoleColor.Red
+        Console.Clear()
+
+        'change text color
+        Console.ForegroundColor() = ConsoleColor.Blue
+
+        'write some text
+        Console.WriteLine("is this red???")
+        Console.WriteLine("the pen is BLUE!!!!!!")
+
+        'play sounds
+        Console.Beep()
+        Console.Beep(200, 1000)
+
+
+        'reset colors to default
+        'Console.ResetColor()
+
         Console.ReadLine()
     End Sub
 
