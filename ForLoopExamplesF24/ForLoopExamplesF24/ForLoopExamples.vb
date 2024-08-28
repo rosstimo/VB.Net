83711527dbb039a56b06d34150a79439c847623c
rosstimo <rosstimo@isu.edu>
2024-08-28 08:33:55 -0600

    add for loop examples

--- ForLoopExamplesF24/ForLoopExamplesF24/ForLoopExamples.vb
+++ ForLoopExamplesF24/ForLoopExamplesF24/ForLoopExamples.vb
@@ -1,7 +1,27 @@
-﻿Module ForLoopExamples
+﻿Option Strict On
+Option Explicit On
+
+Module ForLoopExamples
 
     Sub Main()
+        'X = 5
+        'Console.WriteLine(X)
 
+
+        'For i = 0 To 10 'Step 0.5
+        '    Console.WriteLine(i)
+        'Next
+
+        For row = 0 To 9
+            For column = 0 To 9
+                Console.Write(row & column & " ")
+            Next
+            Console.WriteLine()
+        Next
+
+
+        Console.ReadLine()
     End Sub
 
 End Module
+
