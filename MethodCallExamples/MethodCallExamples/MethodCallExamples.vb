a12a3e4dfdd240370020c836592e69ea5846bdd8
rosstimo <rosstimo@isu.edu>
2024-08-29 08:26:04 -0600

    init

---
+++ MethodCallExamples/MethodCallExamples/MethodCallExamples.vb
@@ -0,0 +1,7 @@
+﻿Module MethodCallExamples
+
+    Sub Main()
+
+    End Sub
+
+End Module
---
+++ MethodCallExamples/MethodCallExamples/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("MethodCallExamples")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("Idaho State University College of Technology")>
+<Assembly: AssemblyProduct("MethodCallExamples")>
+<Assembly: AssemblyCopyright("Copyright © Idaho State University College of Technology 2024")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("b01d95ea-2e40-4be5-8a02-08e6c10336ea")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
