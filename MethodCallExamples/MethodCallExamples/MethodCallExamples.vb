23e995372697253c1e8bd85551689a71b88c2af4
rosstimo <rosstimo@isu.edu>
2024-09-03 09:00:05 -0600

    add ByRef and ByVal example

--- MethodCallExamples/MethodCallExamples/MethodCallExamples.vb
+++ MethodCallExamples/MethodCallExamples/MethodCallExamples.vb
@@ -1,7 +1,10 @@
-﻿Module MethodCallExamples
+﻿Option Compare Text
+
+Module MethodCallExamples
 
     Sub Main()
         Dim firstNumber As Integer = 7
+        Dim secondNumber As Integer = 5
         Console.WriteLine("start of main")
 
         'For i = 1 To 5
@@ -15,9 +18,19 @@
         'Console.WriteLine(firstNumber)
 
 
-        For i = 1 To 10
-            Console.WriteLine(GetRandomNumber())
-        Next
+        'For i = 1 To 100
+        '    Console.WriteLine(GetRandomNumber())
+        'Next
+
+        'Console.WriteLine(SumOf(3, 5))
+
+        Console.WriteLine(firstNumber)
+        Console.WriteLine(secondNumber)
+
+        PlayWithScope(firstNumber, secondNumber)
+
+        Console.WriteLine(firstNumber)
+        Console.WriteLine(secondNumber)
 
         Console.WriteLine("end of main")
         Console.ReadLine()
@@ -45,5 +58,25 @@
         Dim value As Integer = CInt(Int((6 * Rnd()) + 1))
         Return value
     End Function
+
+    ''' <summary>
+    ''' Calculates the sum of two whole numbers
+    ''' </summary>
+    ''' <param name="firstNumber"></param>
+    ''' <param name="secondNumber"></param>
+    ''' <returns>Integer</returns>
+    Function SumOf(firstNumber As Integer, secondNumber As Integer) As Integer
+        Dim result As Integer
+        result = firstNumber + secondNumber
+        Return result
+    End Function
+
+    Sub PlayWithScope(ByVal firstNumber As Integer, ByRef secondNumber As Integer)
+        Console.WriteLine("Start PlayWithScope Sub")
+        firstNumber = 25
+        secondNumber = 42
+        Console.WriteLine("End PlayWithScope Sub")
+    End Sub
+
 
 End Module
