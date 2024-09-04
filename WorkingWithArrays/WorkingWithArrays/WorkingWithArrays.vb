2b15c43a08ef11f567c68effc1f5c827a7c6101d
rosstimo <rosstimo@isu.edu>
2024-09-04 08:57:42 -0600

    add loop

--- WorkingWithArrays/WorkingWithArrays/WorkingWithArrays.vb
+++ WorkingWithArrays/WorkingWithArrays/WorkingWithArrays.vb
@@ -7,6 +7,13 @@
         names(0) = "jimmy"
         names(3) = "Bob"
 
+        'crashes program
+        'Console.WriteLine(names(6))
+
+        For i = 0 To 10
+            Console.WriteLine(names(i))
+        Next
+
         Console.Read()
 
     End Sub
