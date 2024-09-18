79f6e816565f04a61115a95695fd8f56fb349ef0
tim <rosstimo@isu.edu>
2024-09-18 08:12:07 -0600

    fix rename bug

--- WinFormExampleF24/WinFormExampleF24/My Project/Application.Designer.vb
+++ WinFormExampleF24/WinFormExampleF24/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
 
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()> _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.WinFormExampleF24.Form1
+            Me.MainForm = Global.WinFormExampleF24.WinFormExampleForm
         End Sub
     End Class
 End Namespace
