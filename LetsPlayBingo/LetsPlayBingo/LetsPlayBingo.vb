bc29aae3bb4fdb9639812a99203d4c59d1fc5d2e
tim <rosstimo@isu.edu>
2024-09-26 08:35:19 -0600

    working draw ball

--- LetsPlayBingo/LetsPlayBingo/LetsPlayBingo.vb
+++ LetsPlayBingo/LetsPlayBingo/LetsPlayBingo.vb
@@ -1,14 +1,48 @@
-﻿Module LetsPlayBingo
+﻿Option Strict On
+Option Explicit On
+Option Compare Text
+
+Module LetsPlayBingo
 
     Sub Main()
-        Dim BallCage(14, 4) As Boolean
+        Dim ballCage(14, 4) As Boolean
+        Dim done As Boolean = False
+        Dim userInput As String
+
+        Do
+            Display(ballCage)
+            DrawBall(ballCage)
+            userInput = Console.ReadLine()
+            If userInput = "q" Then done = True
+            Console.Clear()
+        Loop Until done
+
+        Console.WriteLine("Have a Nice Day!!!")
+
+        Console.Read()
+    End Sub
+
+    Sub DrawBall(ByRef ballCage(,) As Boolean)
+        Dim _number As Integer
+        Dim _letter As Integer
+
+        Do
+            _number = GetRandomNumberInRange(14, 0)
+            _letter = GetRandomNumberInRange(4, 0)
+        Loop Until Not ballCage(_number, _letter)
+
+        ballCage(_number, _letter) = True
+
+    End Sub
+
+    ''' <summary>
+    ''' Display the formatted game status on the console
+    ''' </summary>
+    ''' <param name="ballCage"></param>
+    Sub Display(ByRef ballCage(,) As Boolean)
         Dim prettyNumber As String
         Dim header() As String = {"B", "I", "N", "G", "O"}
         Dim pad As Integer = 3
-        BallCage(0, 0) = True
-        BallCage(1, 4) = True
-        BallCage(3, 2) = True
-        BallCage(7, 3) = True
         Console.Write("| ")
         For i = 0 To 4
             Console.Write(header(i).PadLeft(pad) & " |")
@@ -19,22 +53,21 @@
         For number = 0 To 14
             Console.Write("| ")
             For letter = 0 To 4
+
                 ' Console.Write(BallCage(number, letter)) 'TODO write pretty number if drawn blank if not
-                Select Case BallCage(number, letter)
+                Select Case ballCage(number, letter)
                     Case True
                         prettyNumber = CStr((15 * (letter)) + 1 + number)
                     Case False
                         prettyNumber = ""
                 End Select
+
                 Console.Write(prettyNumber.PadLeft(pad) & " |")
             Next
             Console.WriteLine()
         Next
 
-        Console.Read()
     End Sub
-
-    'TODO display sub
 
     ''' <summary>
     ''' get a random integer within the specified range. Inclusive
