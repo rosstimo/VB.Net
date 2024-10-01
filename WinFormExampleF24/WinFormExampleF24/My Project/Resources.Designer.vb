7a4cc9f0787db187abd811d48715f14d21f856e7
tim <rosstimo@isu.edu>
2024-10-01 08:12:16 -0600

    add simple splash screen

--- WinFormExampleF24/WinFormExampleF24/My Project/Resources.Designer.vb
+++ WinFormExampleF24/WinFormExampleF24/My Project/Resources.Designer.vb
@@ -63,6 +63,16 @@ Namespace My.Resources
         '''<summary>
         '''  Looks up a localized resource of type System.Drawing.Bitmap.
         '''</summary>
+        Friend ReadOnly Property code_8779047_1280() As System.Drawing.Bitmap
+            Get
+                Dim obj As Object = ResourceManager.GetObject("code-8779047_1280", resourceCulture)
+                Return CType(obj,System.Drawing.Bitmap)
+            End Get
+        End Property
+        
+        '''<summary>
+        '''  Looks up a localized resource of type System.Drawing.Bitmap.
+        '''</summary>
         Friend ReadOnly Property kitten() As System.Drawing.Bitmap
             Get
                 Dim obj As Object = ResourceManager.GetObject("kitten", resourceCulture)
--- WinFormExampleF24/WinFormExampleF24/SplashForm.Designer.vb
+++ WinFormExampleF24/WinFormExampleF24/SplashForm.Designer.vb
@@ -22,9 +22,21 @@ Partial Class SplashForm
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Me.SuspendLayout()
+        '
+        'SplashForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = Global.WinFormExampleF24.My.Resources.Resources.code_8779047_1280
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "SplashForm"
+        Me.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "SplashForm"
+        Me.ResumeLayout(False)
+
     End Sub
 End Class
