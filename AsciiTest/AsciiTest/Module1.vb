80060872e5d269077211fccd14c12ac955279c18
tim <rosstimo@isu.edu>
2024-10-02 08:10:17 -0600

    init

---
+++ AsciiTest/AsciiTest/Module1.vb
@@ -0,0 +1,22 @@
+﻿Module Module1
+
+    Sub Main()
+        'Console.WriteLine("Hello World!")
+        ' change the console font to courier new
+        'Console.OutputEncoding = System.Text.Encoding.ASCII
+
+        Console.Write($"hearts: {ChrW(&H2665)}, ")
+        Console.Write($"spades: {ChrW(&H2660)}, ")
+        Console.Write($"diamonds: {ChrW(&H2666)}, ")
+        Console.Write($"clubs: {ChrW(&H2663)}")
+        'For i = 0 To 255
+        '    Try
+        '        Console.WriteLine($"{i}: {Chr(i)}")
+        '    Catch ex As Exception
+
+        '    End Try
+        'Next
+        Console.Read()
+    End Sub
+
+End Module
