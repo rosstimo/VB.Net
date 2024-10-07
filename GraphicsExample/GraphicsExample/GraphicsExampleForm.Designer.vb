9f9a00f1e4ff37ac6d6f6297c261c8b5cc565860
tim <rosstimo@isu.edu>
2024-10-07 08:48:34 -0600

    add mouse examples

--- GraphicsExample/GraphicsExample/GraphicsExampleForm.Designer.vb
+++ GraphicsExample/GraphicsExample/GraphicsExampleForm.Designer.vb
@@ -22,17 +22,69 @@ Partial Class GraphicsExampleForm
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.DrawingPictureBox = New System.Windows.Forms.PictureBox()
+        Me.GroupBox = New System.Windows.Forms.GroupBox()
+        Me.ExitButton = New System.Windows.Forms.Button()
+        Me.ClearButton = New System.Windows.Forms.Button()
+        CType(Me.DrawingPictureBox, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox.SuspendLayout()
         Me.SuspendLayout()
+        '
+        'DrawingPictureBox
+        '
+        Me.DrawingPictureBox.Cursor = System.Windows.Forms.Cursors.Cross
+        Me.DrawingPictureBox.Location = New System.Drawing.Point(12, 22)
+        Me.DrawingPictureBox.Name = "DrawingPictureBox"
+        Me.DrawingPictureBox.Size = New System.Drawing.Size(509, 259)
+        Me.DrawingPictureBox.TabIndex = 0
+        Me.DrawingPictureBox.TabStop = False
+        '
+        'GroupBox
+        '
+        Me.GroupBox.Controls.Add(Me.ClearButton)
+        Me.GroupBox.Controls.Add(Me.ExitButton)
+        Me.GroupBox.Location = New System.Drawing.Point(12, 287)
+        Me.GroupBox.Name = "GroupBox"
+        Me.GroupBox.Size = New System.Drawing.Size(509, 78)
+        Me.GroupBox.TabIndex = 1
+        Me.GroupBox.TabStop = False
+        '
+        'ExitButton
+        '
+        Me.ExitButton.Location = New System.Drawing.Point(428, 19)
+        Me.ExitButton.Name = "ExitButton"
+        Me.ExitButton.Size = New System.Drawing.Size(75, 43)
+        Me.ExitButton.TabIndex = 0
+        Me.ExitButton.Text = "E&xit"
+        Me.ExitButton.UseVisualStyleBackColor = True
+        '
+        'ClearButton
+        '
+        Me.ClearButton.Location = New System.Drawing.Point(347, 19)
+        Me.ClearButton.Name = "ClearButton"
+        Me.ClearButton.Size = New System.Drawing.Size(75, 43)
+        Me.ClearButton.TabIndex = 1
+        Me.ClearButton.Text = "&Clear"
+        Me.ClearButton.UseVisualStyleBackColor = True
         '
         'GraphicsExampleForm
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(539, 377)
+        Me.Controls.Add(Me.GroupBox)
+        Me.Controls.Add(Me.DrawingPictureBox)
+        Me.Margin = New System.Windows.Forms.Padding(2)
         Me.Name = "GraphicsExampleForm"
         Me.Text = "Form1"
+        CType(Me.DrawingPictureBox, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox.ResumeLayout(False)
         Me.ResumeLayout(False)
 
     End Sub
 
+    Friend WithEvents DrawingPictureBox As PictureBox
+    Friend WithEvents GroupBox As GroupBox
+    Friend WithEvents ClearButton As Button
+    Friend WithEvents ExitButton As Button
 End Class
--- GraphicsExample/GraphicsExample/GraphicsExampleForm.vb
+++ GraphicsExample/GraphicsExample/GraphicsExampleForm.vb
@@ -1,14 +1,14 @@
 ﻿Public Class GraphicsExampleForm
 
     Private Sub GraphicsExampleForm_Click(sender As Object, e As EventArgs) Handles Me.Click
-        'DrawRectangle()
-        'DrawCircle()
-        'DrawText()
+        DrawRectangle()
+        DrawCircle()
+        DrawText()
         DrawImage()
     End Sub
 
     Sub DrawLine()
-        Dim g As Graphics = Me.CreateGraphics()
+        Dim g As Graphics = DrawingPictureBox.CreateGraphics()
         Dim pen As New Pen(Color.Blue)
 
         g.DrawLine(pen, 100, 100, 300, 300)
@@ -18,7 +18,7 @@
     End Sub
 
     Sub DrawRectangle()
-        Dim g As Graphics = Me.CreateGraphics()
+        Dim g As Graphics = DrawingPictureBox.CreateGraphics()
         Dim pen As New Pen(Color.Blue)
 
         g.DrawRectangle(pen, 100, 100, 300, 300)
@@ -27,7 +27,7 @@
     End Sub
 
     Sub DrawCircle()
-        Dim g As Graphics = Me.CreateGraphics()
+        Dim g As Graphics = DrawingPictureBox.CreateGraphics()
         Dim pen As New Pen(Color.Blue)
 
         g.DrawEllipse(pen, 100, 100, 300, 300)
@@ -36,7 +36,7 @@
     End Sub
 
     Sub DrawText()
-        Dim g As Graphics = Me.CreateGraphics()
+        Dim g As Graphics = DrawingPictureBox.CreateGraphics()
         Dim pen As New Pen(Color.Blue)
         Dim brush As New SolidBrush(Color.Red)
 
@@ -46,12 +46,39 @@
     End Sub
 
     Sub DrawImage()
-        Dim g As Graphics = Me.CreateGraphics()
+        Dim g As Graphics = DrawingPictureBox.CreateGraphics()
         Dim image As Image = Image.FromFile("c:\Users\tim\Downloads\kitten.jpg")
         Dim newImage As New Bitmap(image, 500, 500)
 
         g.DrawImage(newImage, 150, 0)
 
         g.Dispose()
+    End Sub
+
+    Sub MouseDraw(startX As Integer, startY As Integer, endX As Integer, endY As Integer)
+        Dim g As Graphics = DrawingPictureBox.CreateGraphics()
+        Dim pen As New Pen(Color.Blue)
+
+        g.DrawLine(pen, startX, startY, endX, endY)
+
+        g.Dispose()
+    End Sub
+
+    ' Event Handlers below here --------------------------------------------------------
+
+    Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
+        Me.Close()
+    End Sub
+
+    Private Sub DrawingPictureBox_MouseMove(sender As Object, e As MouseEventArgs) Handles DrawingPictureBox.MouseDown, DrawingPictureBox.MouseMove
+        Me.Text = $"({e.X.ToString},{e.Y.ToString}) Button: {e.Button}"
+        Static oldX%, oldY%
+        If e.Button = MouseButtons.Left Then
+            MouseDraw(oldX, oldY, e.X, e.Y)
+        End If
+
+        oldX = e.X
+        oldY = e.Y
+
     End Sub
 End Class
