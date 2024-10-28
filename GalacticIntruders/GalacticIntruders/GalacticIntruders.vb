0b0b371ba4baecc5bb504f9f9a1852fd8ebe38cf
tim <rosstimo@isu.edu>
2024-10-28 08:21:00 -0600

    init

---
+++ GalacticIntruders/GalacticIntruders/GalacticIntruders.vb
@@ -0,0 +1,21 @@
+﻿Option Strict On
+Option Explicit On
+
+'TODO
+'[ ] basic animation
+' - [ ] enemy movement
+' - [ ] player movement
+' - [ ] projectile
+'[ ] collision detection
+'[ ] score keeping
+'[ ] lives tracking
+'[ ] start screen
+'[ ] end screen
+'
+Module GalacticIntruders
+
+    Sub Main()
+
+    End Sub
+
+End Module
