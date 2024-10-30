39cf224a3508e399cae0117076f01e45672f9779
tim <rosstimo@isu.edu>
2024-10-30 08:35:47 -0600

    add dynamically sized empty frame

--- GalacticIntruders/GalacticIntruders/GalacticIntruders.vb
+++ GalacticIntruders/GalacticIntruders/GalacticIntruders.vb
@@ -17,16 +17,39 @@ Imports System.Threading.Thread
 Module GalacticIntruders
 
     Sub Main()
+        'Console.WriteLine($"H:{Console.WindowHeight} W:{Console.WindowWidth}")
+        Console.Title = "Galactic Intruders!!!"
 
-        UpdateFrame(Enemy(), 0, 0)
+        UpdateFrame(Enemy(), 1, 1)
 
 
         Console.Read()
 
     End Sub
 
+    Function EmptyFrame() As String(,)
+        Dim Height% = Console.WindowHeight - 2, Width% = Console.WindowWidth - 2
+        Dim frame(Width, Height) As String
+
+        'preload frame
+        For row = 0 To Height
+            For column = 0 To Width
+                If column = 0 Or column = Width Then
+                    frame(column, row) = "|"
+                ElseIf row = 0 Or row = Height Then
+                    frame(column, row) = "_"
+                Else
+                    frame(column, row) = "." 'replace "." with space after debug
+                End If
+            Next
+        Next
+        Return frame
+
+    End Function
+
     Function UpdateFrame(sprite() As String, x As Integer, y As Integer) As String(,)
-        Dim frame(120, 30) As String
+        'Dim frame(120, 30) As String
+        Dim frame(,) As String = EmptyFrame()
 
         For s = 0 To 1
             For i = 0 To sprite(s).Length - 1
@@ -36,8 +59,8 @@ Module GalacticIntruders
         Next
 
 
-        For row = 0 To 15
-            For column = 0 To 120
+        For row = 0 To frame.GetUpperBound(1)
+            For column = 0 To frame.GetUpperBound(0)
 
                 Console.Write(frame(column, row))
             Next
