7530f8822bed24db0e306920242504bc021c957a
tim <rosstimo@isu.edu>
2025-02-03 08:57:27 -0700

    add variable scope and lifetime

--- MethodExamples/MethodExamples/MethodExamples.vb
+++ MethodExamples/MethodExamples/MethodExamples.vb
@@ -3,17 +3,22 @@ Option Strict On
 
 Module MethodExamples
     Sub Main()
+        Dim myLuckyNumber As Integer = 7
 
         Console.WriteLine("Hello from Sub Main!!")
-        For i = 1 To 10
-            MyOtherSub()
-        Next
+        Console.WriteLine($"My lucky number is {myLuckyNumber}!")
+        MyOtherSub()
+        Console.WriteLine($"My lucky number is {myLuckyNumber}!")
 
     End Sub
 
 
     Sub MyOtherSub()
+        Dim myLuckyNumber As Integer = 5
         Console.WriteLine("Hello from MyOtherSub!!")
+        Console.WriteLine($"My lucky number is {myLuckyNumber}!")
+
+
     End Sub
 
 
