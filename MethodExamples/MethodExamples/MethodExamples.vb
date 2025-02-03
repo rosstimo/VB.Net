d470a6a14e9cee49dce15a57ca1277310c73633e
tim <rosstimo@isu.edu>
2025-02-03 08:46:33 -0700

    add simple sub example

--- MethodExamples/MethodExamples/MethodExamples.vb
+++ MethodExamples/MethodExamples/MethodExamples.vb
@@ -4,6 +4,17 @@ Option Strict On
 Module MethodExamples
     Sub Main()
 
+        Console.WriteLine("Hello from Sub Main!!")
+        For i = 1 To 10
+            MyOtherSub()
+        Next
+
     End Sub
+
+
+    Sub MyOtherSub()
+        Console.WriteLine("Hello from MyOtherSub!!")
+    End Sub
+
 
 End Module
