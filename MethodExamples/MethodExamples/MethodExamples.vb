accc7086d82c6e585f8a6e00d0ebbe6972fa0d56
tim <rosstimo@isu.edu>
2025-02-04 08:25:57 -0700

    add simple ByVal Sub argument example

--- MethodExamples/MethodExamples/MethodExamples.vb
+++ MethodExamples/MethodExamples/MethodExamples.vb
@@ -7,18 +7,29 @@ Module MethodExamples
 
         Console.WriteLine("Hello from Sub Main!!")
         Console.WriteLine($"My lucky number is {myLuckyNumber}!")
-        MyOtherSub()
+        Console.WriteLine()
+        ' MyOtherSub()
+        Sandwich(myLuckyNumber)
+
+        Console.WriteLine("Hello from Sub Main!!")
         Console.WriteLine($"My lucky number is {myLuckyNumber}!")
+        Console.WriteLine()
 
     End Sub
-
 
     Sub MyOtherSub()
         Dim myLuckyNumber As Integer = 5
         Console.WriteLine("Hello from MyOtherSub!!")
         Console.WriteLine($"My lucky number is {myLuckyNumber}!")
+        Console.WriteLine()
+
+    End Sub
 
 
+    Sub Sandwich(ByVal myLuckyNumber As Integer)
+        Console.WriteLine("Hello from Sandwich!!")
+        Console.WriteLine($"My lucky number is {myLuckyNumber}!")
+        Console.WriteLine()
     End Sub
 
 
