2c8b3ef55521b6382ed3b7abeb09b66afd66e6b2
tim <rosstimo@isu.edu>
2025-02-05 08:49:34 -0700

    add function examples

--- MethodExamples/MethodExamples/MethodExamples.vb
+++ MethodExamples/MethodExamples/MethodExamples.vb
@@ -5,20 +5,35 @@ Module MethodExamples
     Sub Main()
         Dim myLuckyNumber As Integer = 7
 
-        Console.WriteLine("Hello from Sub Main!!")
-        Console.WriteLine($"My lucky number is {myLuckyNumber}!")
-        Console.WriteLine()
+        'Console.WriteLine("Hello from Sub Main!!")
+        'Console.WriteLine($"My lucky number is {myLuckyNumber}!")
+        'Console.WriteLine()
 
-        'MyOtherSub()
-        'Sandwich(myLuckyNumber)
-        Marine(myLuckyNumber)
+        ''MyOtherSub()
+        ''Sandwich(myLuckyNumber)
+        ''Marine(myLuckyNumber)
 
-        Console.WriteLine("Hello from Sub Main!!")
-        Console.WriteLine($"My lucky number is {myLuckyNumber}!")
-        Console.WriteLine()
+        'Console.WriteLine("Hello from Sub Main!!")
+        'Console.WriteLine($"My lucky number is {myLuckyNumber}!")
+        'Console.WriteLine()
+
+        'Console.WriteLine(MyActualLuckyNumber())
+
+        'Console.WriteLine(SumOf(5, 7))
+
+        'Randomize()
+        'For i = 1 To 10
+        '    Console.WriteLine(Rnd())
+        'Next
+
+        For i = 1 To 100
+            Console.WriteLine(RandomNumber())
+        Next
+
 
     End Sub
 
+    'Sub Examples *************************************************************
     Sub MyOtherSub()
         Dim myLuckyNumber As Integer = 5
         Console.WriteLine("Hello from MyOtherSub!!")
@@ -26,7 +41,6 @@ Module MethodExamples
         Console.WriteLine()
 
     End Sub
-
 
     Sub Sandwich(ByVal myLuckyNumber As Integer)
         myLuckyNumber += 5
@@ -43,5 +57,26 @@ Module MethodExamples
 
     End Sub
 
+    'Function Examples ********************************************************
+
+    Function MyActualLuckyNumber() As Integer
+        Return 77
+    End Function
+
+    Function SumOf(firstNumber As Integer, secondNumber As Integer) As Integer
+        Dim result As Integer
+        result = firstNumber + secondNumber
+        Return result
+    End Function
+
+    Function RandomNumber() As Integer
+        Dim myRandomNumber As Single
+        Randomize()
+
+        myRandomNumber = Rnd() * 10
+        myRandomNumber = Int(myRandomNumber) + 1
+
+        Return CInt(myRandomNumber)
+    End Function
 
 End Module
