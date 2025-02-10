0e423ff236efb4d9fb2d8a77c941fe66ca89111b
tim <rosstimo@isu.edu>
2025-02-10 08:26:47 -0700

    add simple array example

--- ArrayExamples/ArrayExamples/ArrayExamples.vb
+++ ArrayExamples/ArrayExamples/ArrayExamples.vb
@@ -1,6 +1,25 @@
-﻿Module ArrayExamples
+﻿'header
+
+Option Strict On
+Option Explicit On
+
+Module ArrayExamples
 
     Sub Main()
+
+        'arrays are cools
+        Dim fruits(5) As String
+
+        fruits(0) = "banana"
+
+        fruits(3) = "grape"
+
+        'fruits(6) = "apple"
+
+        Console.WriteLine()
+
+
+
 
     End Sub
 
