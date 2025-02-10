5270e6ce384d1b4d97dfe56ac3db03fd4647b73a
tim <rosstimo@isu.edu>
2025-02-10 08:09:36 -0700

    init

--- ArrayExamples/ArrayExamples/My Project/AssemblyInfo.vb
+++ ArrayExamples/ArrayExamples/My Project/AssemblyInfo.vb
@@ -12,13 +12,13 @@ Imports System.Runtime.InteropServices
 <Assembly: AssemblyDescription("")>
 <Assembly: AssemblyCompany("")>
 <Assembly: AssemblyProduct("ArrayExamples")>
-<Assembly: AssemblyCopyright("Copyright ©  2022")>
+<Assembly: AssemblyCopyright("Copyright ©  2025")>
 <Assembly: AssemblyTrademark("")>
 
 <Assembly: ComVisible(False)>
 
 'The following GUID is for the ID of the typelib if this project is exposed to COM
-<Assembly: Guid("749b846f-3406-420d-9bf9-48ba2bd32289")>
+<Assembly: Guid("ae0e59b5-2755-4f3d-8a13-aae12cd48c2b")>
 
 ' Version information for an assembly consists of the following four values:
 '
@@ -27,9 +27,6 @@ Imports System.Runtime.InteropServices
 '      Build Number
 '      Revision
 '
-' You can specify all the values or you can default the Build and Revision Numbers
-' by using the '*' as shown below:
-' <Assembly: AssemblyVersion("1.0.*")>
 
 <Assembly: AssemblyVersion("1.0.0.0")>
 <Assembly: AssemblyFileVersion("1.0.0.0")>
