e941cd0513527fec192162b885e7d23fc55c3133
tim <rosstimo@isu.edu>
2025-02-27 08:57:18 -0700

    fix prompt

--- BingoGame/BingoGame/BingoGame.vb
+++ BingoGame/BingoGame/BingoGame.vb
@@ -19,12 +19,16 @@ Module BingoGame
 
     Sub Main()
         Dim userInput As String
+        Dim _lastBall(1) As Integer
 
         Do
             Console.Clear()
             DisplayBoard()
             Console.WriteLine()
             'prompt
+            Console.WriteLine("Enter D to draw a ball, c for new game, or q to quit.")
+            _lastBall = LastBall()
+            Console.WriteLine($"The last ball was {FormatBallLetter(_lastBall(1))} {FormatBallNumber(_lastBall(0), _lastBall(1))}")
             userInput = Console.ReadLine()
             Select Case userInput
                 Case "d"
@@ -60,16 +64,27 @@ Module BingoGame
             'mark current ball as being drawn, updates the display
             BingoTracker(currentBallNumber, currentBallLetter, True)
             ballCounter += 1
+            LastBall(currentBallNumber, currentBallLetter)
 
             'for debug write valid ball draws to console
-            Console.WriteLine($"the current row is {currentBallNumber} and column is {currentBallLetter}")
+            'Console.WriteLine($"the current row Is {currentBallNumber} And column Is {currentBallLetter}")
 
         End If
 
     End Sub
 
-    ''' <summary>
-    ''' Contains a persistent array that tracks all possible bingo balls
+    Function LastBall(Optional ballNumber As Integer = -1, Optional ballLetter As Integer = -1) As Integer()
+        Static _lastBall(1) As Integer
+
+        If ballNumber <> -1 Then
+            _lastBall(0) = ballNumber
+            _lastBall(1) = ballLetter
+        End If
+
+        Return _lastBall
+    End Function
+
+    ''' <summary> ent array that tracks all possible bingo balls
     ''' and whether they have been drawn during the current game.
     ''' </summary>
     ''' <param name="ballNumber"></param>
@@ -98,26 +113,28 @@ Module BingoGame
     ''' Iterates through the tracker array and displays bingo board to the console.
     ''' </summary>
     Sub DisplayBoard()
-        Dim temp As String = " |"
+        Dim displayString As String = "|"
         Dim heading() As String = {"B", "I", "N", "G", "O"}
         Dim tracker(,) As Boolean = BingoTracker(0, 0)
+        Dim columnWidth As Integer = 5
         For Each letter In heading
-            Console.Write(letter.PadLeft(2).PadRight(4))
+            'Console.Write(letter.PadLeft((columnWidth \ 2) + 1).PadRight(columnWidth))
+            Console.Write(letter.PadLeft(CInt(Math.Ceiling(columnWidth / 2))).PadRight(columnWidth))
         Next
 
         Console.WriteLine()
-        Console.WriteLine(StrDup(20, "_"))
+        Console.WriteLine(StrDup(columnWidth * 5, "_"))
         For currentNumber = 0 To 14
             For currentLetter = 0 To 4
 
                 If tracker(currentNumber, currentLetter) Then
-                    temp = $"{FormatBallNumber(currentNumber, currentLetter)} |" 'display for drawn balls
-                Else
-                    temp = " |" 'display for not drawn balls
+                    displayString = $"{FormatBallNumber(currentNumber, currentLetter)} |" 'display for drawn balls
+            Else
+                    displayString = "|" 'display for not drawn balls
                 End If
 
-                temp = temp.PadLeft(4)
-                Console.Write(temp)
+                displayString = displayString.PadLeft(columnWidth)
+                Console.Write(displayString)
 
             Next
 
@@ -130,6 +147,23 @@ Module BingoGame
         Dim _ballNumber As String
         _ballNumber = CStr((ballNumber + 1) + (ballLetter * 15))
         Return _ballNumber
+    End Function
+
+    Function FormatBallLetter(ballLetter As Integer) As String
+        Dim _ballLetter As String
+        Select Case ballLetter
+            Case 0
+                _ballLetter = "B"
+            Case 1
+                _ballLetter = "I"
+            Case 2
+                _ballLetter = "N"
+            Case 3
+                _ballLetter = "G"
+            Case 4
+                _ballLetter = "O"
+        End Select
+        Return _ballLetter
     End Function
     Function RandomNumberBetween(min As Integer, max As Integer) As Integer
         Dim temp As Single
