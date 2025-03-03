6681005fd062a73e1e3da951a725a1416b0b5788
tim <rosstimo@isu.edu>
2025-03-03 08:52:14 -0700

    add labels and text boxes

--- WinFormExample/WinFormExample/WinFormExampleForm.Designer.vb
+++ WinFormExample/WinFormExample/WinFormExampleForm.Designer.vb
@@ -24,6 +24,12 @@ Partial Class WinFormExampleForm
     Private Sub InitializeComponent()
         Me.ExitButton = New System.Windows.Forms.Button()
         Me.UpdateButton = New System.Windows.Forms.Button()
+        Me.FirstLabel = New System.Windows.Forms.Label()
+        Me.FirstTextBox = New System.Windows.Forms.TextBox()
+        Me.LastTextBox = New System.Windows.Forms.TextBox()
+        Me.LastLabel = New System.Windows.Forms.Label()
+        Me.AgeTextBox = New System.Windows.Forms.TextBox()
+        Me.AgeLabel = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'ExitButton
@@ -31,8 +37,8 @@ Partial Class WinFormExampleForm
         Me.ExitButton.Location = New System.Drawing.Point(594, 347)
         Me.ExitButton.Name = "ExitButton"
         Me.ExitButton.Size = New System.Drawing.Size(163, 91)
-        Me.ExitButton.TabIndex = 1
-        Me.ExitButton.Text = "Exit"
+        Me.ExitButton.TabIndex = 10
+        Me.ExitButton.Text = "E&xit"
         Me.ExitButton.UseVisualStyleBackColor = True
         '
         'UpdateButton
@@ -40,23 +46,84 @@ Partial Class WinFormExampleForm
         Me.UpdateButton.Location = New System.Drawing.Point(425, 347)
         Me.UpdateButton.Name = "UpdateButton"
         Me.UpdateButton.Size = New System.Drawing.Size(163, 91)
-        Me.UpdateButton.TabIndex = 0
-        Me.UpdateButton.Text = "Update"
+        Me.UpdateButton.TabIndex = 9
+        Me.UpdateButton.Text = "&Update"
         Me.UpdateButton.UseVisualStyleBackColor = True
+        '
+        'FirstLabel
+        '
+        Me.FirstLabel.AutoSize = True
+        Me.FirstLabel.Location = New System.Drawing.Point(1, 36)
+        Me.FirstLabel.Name = "FirstLabel"
+        Me.FirstLabel.Size = New System.Drawing.Size(86, 20)
+        Me.FirstLabel.TabIndex = 2
+        Me.FirstLabel.Text = "First Name"
+        '
+        'FirstTextBox
+        '
+        Me.FirstTextBox.Location = New System.Drawing.Point(93, 33)
+        Me.FirstTextBox.Name = "FirstTextBox"
+        Me.FirstTextBox.Size = New System.Drawing.Size(241, 26)
+        Me.FirstTextBox.TabIndex = 0
+        '
+        'LastTextBox
+        '
+        Me.LastTextBox.Location = New System.Drawing.Point(93, 65)
+        Me.LastTextBox.Name = "LastTextBox"
+        Me.LastTextBox.Size = New System.Drawing.Size(241, 26)
+        Me.LastTextBox.TabIndex = 1
+        '
+        'LastLabel
+        '
+        Me.LastLabel.AutoSize = True
+        Me.LastLabel.Location = New System.Drawing.Point(1, 68)
+        Me.LastLabel.Name = "LastLabel"
+        Me.LastLabel.Size = New System.Drawing.Size(86, 20)
+        Me.LastLabel.TabIndex = 4
+        Me.LastLabel.Text = "Last Name"
+        '
+        'AgeTextBox
+        '
+        Me.AgeTextBox.Location = New System.Drawing.Point(93, 97)
+        Me.AgeTextBox.Name = "AgeTextBox"
+        Me.AgeTextBox.Size = New System.Drawing.Size(91, 26)
+        Me.AgeTextBox.TabIndex = 2
+        '
+        'AgeLabel
+        '
+        Me.AgeLabel.AutoSize = True
+        Me.AgeLabel.Location = New System.Drawing.Point(49, 100)
+        Me.AgeLabel.Name = "AgeLabel"
+        Me.AgeLabel.Size = New System.Drawing.Size(38, 20)
+        Me.AgeLabel.TabIndex = 6
+        Me.AgeLabel.Text = "Age"
         '
         'WinFormExampleForm
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(769, 450)
+        Me.Controls.Add(Me.AgeTextBox)
+        Me.Controls.Add(Me.AgeLabel)
+        Me.Controls.Add(Me.LastTextBox)
+        Me.Controls.Add(Me.LastLabel)
+        Me.Controls.Add(Me.FirstTextBox)
+        Me.Controls.Add(Me.FirstLabel)
         Me.Controls.Add(Me.UpdateButton)
         Me.Controls.Add(Me.ExitButton)
         Me.Name = "WinFormExampleForm"
         Me.Text = "Windows Form Example"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
     Friend WithEvents ExitButton As Button
     Friend WithEvents UpdateButton As Button
+    Friend WithEvents FirstLabel As Label
+    Friend WithEvents FirstTextBox As TextBox
+    Friend WithEvents LastTextBox As TextBox
+    Friend WithEvents LastLabel As Label
+    Friend WithEvents AgeTextBox As TextBox
+    Friend WithEvents AgeLabel As Label
 End Class
--- WinFormExample/WinFormExample/WinFormExampleForm.vb
+++ WinFormExample/WinFormExample/WinFormExampleForm.vb
@@ -4,11 +4,20 @@ Option Explicit On
 Option Strict On
 
 Public Class WinFormExampleForm
+    Sub Clear()
+        FirstTextBox.Text = ""
+        LastTextBox.Text = ""
+        AgeTextBox.Text = ""
+    End Sub
+
+    'Event Handlers ***********************************************************
     Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
         Me.Close()
     End Sub
     Private Sub UpdateButton_Click(sender As Object, e As EventArgs) Handles UpdateButton.Click
         'Me.Text = "Forms Are Cool"
-        Me.Text = StrReverse(Me.Text)
+        'Me.Text = StrReverse(Me.Text)
+        Me.Text = FirstTextBox.Text & " " & LastTextBox.Text
+        Clear()
     End Sub
 End Class
