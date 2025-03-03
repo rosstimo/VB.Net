c08ef85ebf9b5ab3070f503c02fa007242220710
tim <rosstimo@isu.edu>
2025-03-03 08:33:50 -0700

    add buttons

--- WinFormExample/WinFormExample/WinFormExampleForm.Designer.vb
+++ WinFormExample/WinFormExample/WinFormExampleForm.Designer.vb
@@ -22,17 +22,41 @@ Partial Class WinFormExampleForm
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.ExitButton = New System.Windows.Forms.Button()
+        Me.UpdateButton = New System.Windows.Forms.Button()
         Me.SuspendLayout()
+        '
+        'ExitButton
+        '
+        Me.ExitButton.Location = New System.Drawing.Point(594, 347)
+        Me.ExitButton.Name = "ExitButton"
+        Me.ExitButton.Size = New System.Drawing.Size(163, 91)
+        Me.ExitButton.TabIndex = 0
+        Me.ExitButton.Text = "Exit"
+        Me.ExitButton.UseVisualStyleBackColor = True
+        '
+        'UpdateButton
+        '
+        Me.UpdateButton.Location = New System.Drawing.Point(425, 347)
+        Me.UpdateButton.Name = "UpdateButton"
+        Me.UpdateButton.Size = New System.Drawing.Size(163, 91)
+        Me.UpdateButton.TabIndex = 1
+        Me.UpdateButton.Text = "Update"
+        Me.UpdateButton.UseVisualStyleBackColor = True
         '
         'WinFormExampleForm
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(769, 450)
+        Me.Controls.Add(Me.UpdateButton)
+        Me.Controls.Add(Me.ExitButton)
         Me.Name = "WinFormExampleForm"
         Me.Text = "Windows Form Example"
         Me.ResumeLayout(False)
 
     End Sub
 
+    Friend WithEvents ExitButton As Button
+    Friend WithEvents UpdateButton As Button
 End Class
--- WinFormExample/WinFormExample/WinFormExampleForm.vb
+++ WinFormExample/WinFormExample/WinFormExampleForm.vb
@@ -4,7 +4,11 @@ Option Explicit On
 Option Strict On
 
 Public Class WinFormExampleForm
-
-    'code goes here
-
+    Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
+        Me.Close()
+    End Sub
+    Private Sub UpdateButton_Click(sender As Object, e As EventArgs) Handles UpdateButton.Click
+        'Me.Text = "Forms Are Cool"
+        Me.Text = StrReverse(Me.Text)
+    End Sub
 End Class
