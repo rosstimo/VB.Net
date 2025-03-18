2bc7e2d2b698162272bb3dcb1ad5d006b404bf65
tim <rosstimo@isu.edu>
2025-03-18 09:00:31 -0600

    add class lib and object examples

--- OOPExamples/OOPExamples/Animal.vb
+++ OOPExamples/OOPExamples/Animal.vb
@@ -1,14 +1,51 @@
 ﻿Public Class Animal
+    'properties -------
 
-    Dim movement As String
+    Private _movement As String
+    Public Property movement() As String
+        Get
+            Return _movement
+        End Get
+        Set(ByVal value As String)
+            _movement = value
+        End Set
+    End Property
+
+    Private _skinType As String
+    Public Property skinType() As String
+        Get
+            Return _skinType
+        End Get
+        Set(ByVal value As String)
+            _skinType = value
+        End Set
+    End Property
+
+    Private _vocalization As String
+    Public Property vocalization() As String
+        Get
+            Return _vocalization
+        End Get
+        Set(ByVal value As String)
+            _vocalization = value
+        End Set
+    End Property
+
+
+    'fields ------------
+    'Dim movement As String
 
     Sub New()
         Me.movement = "Animals can move.. except sponges..."
+        Me.skinType = "I'm not sure if sponges have skin"
     End Sub
-
 
     Sub Move()
         Console.WriteLine(Me.movement)
+    End Sub
+
+    Sub Speak()
+        Console.WriteLine(Me.vocalization)
     End Sub
 
 End Class
--- OOPExamples/OOPExamples/MainModule.vb
+++ OOPExamples/OOPExamples/MainModule.vb
@@ -6,13 +6,29 @@
         'MsgBox(ExampleClass.SumOf(4, 3))
 
         Dim jimmy As New Animal
-        Dim bob As New Animal
+        Dim bob As New Mammal
+        Dim lucky As New Dog
+        Dim pepe As New Dog
+        Dim nemo As New Fish
 
-        jimmy.Move()
-        bob.Move()
 
 
+        'jimmy.Move()
+        'Console.WriteLine(jimmy.skinType)
+        'bob.Move()
+        'Console.WriteLine(bob.skinType)
 
+
+        pepe.movement = "shiver"
+        pepe.skinType = "hairless rat"
+        pepe.vocalization = "yip yip yip"
+        pepe.Speak()
+        pepe.Move()
+
+        lucky.Speak()
+        lucky.Move()
+
+        nemo.Speak()
 
 
 
