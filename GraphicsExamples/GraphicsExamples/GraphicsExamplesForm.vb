e0da121d4d6f281c823cc63032b145adaa6c6475
tim <rosstimo@isu.edu>
2025-03-31 08:51:21 -0600

    add more examples

--- GraphicsExamples/GraphicsExamples/GraphicsExamplesForm.vb
+++ GraphicsExamples/GraphicsExamples/GraphicsExamplesForm.vb
@@ -10,9 +10,40 @@ Public Class GraphicsExamplesForm
         Dim g As Graphics = Me.CreateGraphics
         Dim pen As New Pen(Color.Black)
 
-        'pen.Color = Color.Black
+        'pen.Color = Color.Lime
 
         g.DrawLine(pen, 50, 50, 100, 100)
+
+        g.Dispose()
+    End Sub
+
+    Sub DrawRectangle()
+        Dim g As Graphics = Me.CreateGraphics
+        Dim pen As New Pen(Color.Red, 3)
+
+        g.DrawRectangle(pen, 150, 150, 300, 250)
+
+        g.Dispose()
+    End Sub
+
+    Sub DrawEllipse()
+        Dim g As Graphics = Me.CreateGraphics
+        Dim pen As New Pen(Color.RoyalBlue, 5)
+
+        g.DrawEllipse(pen, 160, 160, 280, 230)
+
+        g.Dispose()
+    End Sub
+
+    Sub DrawString()
+        Dim g As Graphics = Me.CreateGraphics
+        Dim pen As New Pen(Color.Red, 3)
+        ' Create font and brush.
+        Dim drawFont As New Font("Arial", 16)
+        Dim drawBrush As New SolidBrush(Color.Crimson)
+
+
+        g.DrawString("Graphics!", drawFont, drawBrush, 200, 200)
 
         g.Dispose()
     End Sub
@@ -20,6 +51,9 @@ Public Class GraphicsExamplesForm
     ' Event Handlers ----------------------------------------------------------
     Private Sub GraphicsExamplesForm_Click(sender As Object, e As EventArgs) Handles Me.Click
         DrawLine()
+        DrawRectangle()
+        DrawEllipse()
+        DrawString()
     End Sub
 
     Private Sub GraphicsExamplesForm_MouseMove(sender As Object, e As MouseEventArgs) Handles Me.MouseMove
