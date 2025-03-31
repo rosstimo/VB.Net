2499aa64ba6c8f8f00ea5f92242be533270f922e
tim <rosstimo@isu.edu>
2025-03-31 08:30:00 -0600

    add draw line and mouse coords

--- GraphicsExamples/GraphicsExamples/GraphicsExamplesForm.vb
+++ GraphicsExamples/GraphicsExamples/GraphicsExamplesForm.vb
@@ -1,3 +1,28 @@
-﻿Public Class GraphicsExamplesForm
+﻿'Graphics Examples
+'Spring 2025
 
+Option Strict On
+Option Explicit On
+
+Public Class GraphicsExamplesForm
+
+    Sub DrawLine()
+        Dim g As Graphics = Me.CreateGraphics
+        Dim pen As New Pen(Color.Black)
+
+        'pen.Color = Color.Black
+
+        g.DrawLine(pen, 50, 50, 100, 100)
+
+        g.Dispose()
+    End Sub
+
+    ' Event Handlers ----------------------------------------------------------
+    Private Sub GraphicsExamplesForm_Click(sender As Object, e As EventArgs) Handles Me.Click
+        DrawLine()
+    End Sub
+
+    Private Sub GraphicsExamplesForm_MouseMove(sender As Object, e As MouseEventArgs) Handles Me.MouseMove
+        Me.Text = $"({e.X},{e.Y})"
+    End Sub
 End Class
