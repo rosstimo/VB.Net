5e357602d0039fcdf12f17bdea55aacac77513df
tim <rosstimo@isu.edu>
2025-04-01 08:18:17 -0600

    add ForeGroundColor Function

--- GraphicsExamples/GraphicsExamples/GraphicsExamplesForm.vb
+++ GraphicsExamples/GraphicsExamples/GraphicsExamplesForm.vb
@@ -6,11 +6,21 @@ Option Explicit On
 
 Public Class GraphicsExamplesForm
 
+    Function ForeGroundColor(Optional newColor As Color = Nothing) As Color
+        Static _foreColor As Color = Color.Black
+
+        If newColor <> Nothing Then
+            _foreColor = newColor
+        End If
+
+        Return _foreColor
+    End Function
+
     Sub DrawLine()
         Dim g As Graphics = Me.CreateGraphics
         Dim pen As New Pen(Color.Black)
 
-        'pen.Color = Color.Lime
+        pen.Color = ForeGroundColor(Color.Lime)
 
         g.DrawLine(pen, 50, 50, 100, 100)
 
@@ -40,8 +50,7 @@ Public Class GraphicsExamplesForm
         Dim pen As New Pen(Color.Red, 3)
         ' Create font and brush.
         Dim drawFont As New Font("Arial", 16)
-        Dim drawBrush As New SolidBrush(Color.Crimson)
-
+        Dim drawBrush As New SolidBrush(ForeGroundColor)
 
         g.DrawString("Graphics!", drawFont, drawBrush, 200, 200)
 
